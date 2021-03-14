9a5b8a12b04b59c38553b875c71962fb55fbf9f9
Pelle Arn <perarn@kth.se>
2021-03-14 23:06:00 +0100

    changed column system, fixed typo, some css

--- techquiz/src/components/dashboard/PlayerStats.js
+++ techquiz/src/components/dashboard/PlayerStats.js
@@ -2,26 +2,27 @@ import React from 'react';
 
 const PlayerStats = (props) => {
     return(
-        <div className="container section">
+        <div className="dashboard-item-container container section">
+            <h5>Stats:</h5>
             <div className="row">
-                <div className="col s12 m6">
+                <div className="col s12 m7">
                     Wins: 26
                 </div>
-                <div className="col s12 m6">
+                <div className="col s12 m5">
                     Current multiplayer rating: 102
                 </div>
             </div>
             <div className="row">
-                <div className="col s12 m12">
+                <div className="col s12 m7">
                     Losses: 4
+                </div>
+                <div className="col s12 m5">
+                    Lifetime best single score: 5
                 </div>
             </div>
             <div className="row">
-                <div className="col s12 m6">
-                    W/L-ration: 6.5
-                </div>
-                <div className="col s12 m6">
-                    Lifetime best single score: 5
+                <div className="col s12 m12">
+                    W/L-ratio: 6.5
                 </div>
             </div>
         </div>
