d3a20b6f358ca8e7c4bc643b35ab8767d6ee68b7
Pelle Arn <perarn@kth.se>
2021-03-14 18:41:41 +0100

    move the Logo, changed the className temporarily from logo to left

--- techquiz/src/components/layout/NavBar.js
+++ techquiz/src/components/layout/NavBar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import LoggedInLinks from "./LoggedInLinks";
+import LoggedOutLinks from './LoggedOutLinks'
 
 
 const Navbar = (props) => {
     return (
         <nav className="wrapper grey darken-3">
             <div className="container">
-                <Link to='/' className="logo">TechQuiz</Link>
+                <Link to='/' className="left">TechQuiz</Link>
+                
                 <LoggedInLinks />
+                <LoggedOutLinks/>
             </div>
         </nav>
 
