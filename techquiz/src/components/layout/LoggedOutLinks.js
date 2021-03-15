fa9b4e1199237f25362b8dd92dc5284d7c5018f4
Pelle Arn <perarn@kth.se>
2021-03-15 13:01:24 +0100

    linked the navbar to the correct components

--- techquiz/src/components/layout/LoggedOutLinks.js
+++ techquiz/src/components/layout/LoggedOutLinks.js
@@ -4,8 +4,8 @@ import {NavLink} from "react-router-dom";
 const LoggedOutLinks = (props) => {
     return(
         <>
-            <li><NavLink to='/'>Login</NavLink></li>
-            <li><NavLink to='/'>Signup</NavLink></li>
+            <li><NavLink to='/signin'>Login</NavLink></li>
+            <li><NavLink to='/signup'>Signup</NavLink></li>
         </>
     )
 }
