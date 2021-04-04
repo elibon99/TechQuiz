b19836d5cdd933a9f38b520cf2413233355eafa7
bonnici <bonnici@kth.se>
2021-04-04 14:14:54 +0200

    Added some descriptive text to category selection component

--- techquiz/src/components/game/GameVsCategoryInfo.js
+++ techquiz/src/components/game/GameVsCategoryInfo.js
@@ -3,6 +3,10 @@ import React from 'react';
 const GameVsCategoryInfo = () => {
     return(
         <div className="card-content">
+            <div className="row category-selection-title">
+                <h3>Your turn</h3>
+                <h5>Choose category</h5>
+            </div>
             <div className="row flex">
                 <div className="col s12 m5 player-content">
                     <i className="large material-icons">account_circle</i>
@@ -14,7 +18,7 @@ const GameVsCategoryInfo = () => {
                     </h5>
                 </div>
                 <div className="col s12 m2 score-content">
-                    <span>Category Selection</span>
+                    <span>VS</span>
                 </div>
                 <div className="col s12 m5 player-content">
                     <i className="large material-icons">account_circle</i>
