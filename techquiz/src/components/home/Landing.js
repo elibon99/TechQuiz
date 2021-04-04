d9e55df10c0352cbc613bbb5e8b2b2427309dea4
Pelle Arn <perarn@kth.se>
2021-04-04 21:24:57 +0200

    added a homepage/landing for the app

---
+++ techquiz/src/components/home/Landing.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Landing = () => {
+    return (
+        <div className="homepage-container">
+            <p>
+                <span className="homepage-title-1">Welcome to </span>
+                <span className="homepage-title-2">TechQuiz</span>
+            </p>
+            <p className="homepage-title-3">
+                Your multiplayer tech quiz app
+            </p>
+        </div>
+    )
+}
+
+export default Landing;
