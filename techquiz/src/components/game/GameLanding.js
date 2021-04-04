68845dec3ae6c371736d824376dce8d500ed8c25
bonnici <bonnici@kth.se>
2021-04-04 14:01:41 +0200

    Changed structure in game landing

--- techquiz/src/components/game/GameLanding.js
+++ techquiz/src/components/game/GameLanding.js
@@ -1,35 +1,12 @@
 import React from 'react';
 import GameSetItem from "./GameSetItem";
+import GameVsInfo from "./GameVsInfo";
 
 const GameLanding = (props) => {
     return(
         <div className="container">
             <div className="card game-landing-container">
-                <div className="card-content">
-                    <div className="row flex">
-                        <div className="col s12 m5 player-content">
-                            <i className="large material-icons">account_circle</i>
-                            <h5>
-                                Elias
-                            </h5>
-                            <h5>
-                                Rating: 102
-                            </h5>
-                        </div>
-                        <div className="col s12 m2 score-content">
-                            <span>0-0</span>
-                        </div>
-                        <div className="col s12 m5 player-content">
-                            <i className="large material-icons">account_circle</i>
-                            <h5>
-                                Pelle
-                            </h5>
-                            <h5>
-                                Rating: 120
-                            </h5>
-                        </div>
-                    </div>
-                </div>
+                <GameVsInfo/>
                 <GameSetItem/>
                 <div className="row">
                     <div className="container">
