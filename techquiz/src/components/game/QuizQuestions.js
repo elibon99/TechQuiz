7c7093177a501efdd62140fb980ee6f277036311
Pelle Arn <perarn@kth.se>
2021-04-04 15:45:47 +0200

    added a question component showing questions, answers, timer, score, question number

---
+++ techquiz/src/components/game/QuizQuestions.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const answers = [
+    {name: "ls", id: 1},
+    {name: "delete", id: 2},
+    {name: "remove", id: 3},
+    {name: "rmdir", id: 4},
+]
+
+const QuizQuestions = () => {
+    return(
+        <div className="container">
+            <div className="card-content">
+                <div className="row answer-display-container">
+                    <div className="col s6 m6"><h6 className="left">Current Score: 8 (+7)</h6></div>
+                    <div className="col s6 m6"><h6 className="right">Question 1/3</h6></div>
+                    <div className="col s12 m12">
+                        <p className="center">How to delete a directory in Linux?</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <p className="center">Time left: 0:10</p>
+                </div>
+                <div className="row">
+                    {answers.map((answer => {
+                        return (
+                            <div key={answer.id} className="col s12 m6">
+                                <div className="card category-title-container">
+                                    <div className="category-title">
+                                        {answer.name}
+                                    </div>
+                                </div>
+                            </div>)
+                    }))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default QuizQuestions;
