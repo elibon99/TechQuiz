33a1484a1be4c471c1d51af3517770c094bd280d
bonnici <bonnici@kth.se>
2021-04-05 16:45:00 +0200

    Added SignIn presenter

--- techquiz/src/components/authentication/SignIn.js
+++ techquiz/src/components/authentication/SignIn.js
@@ -1,23 +1,33 @@
 import React from 'react'
 
-const SignIn = () => {
+const SignIn = (props) => {
+    const [credentials, setCredentials] = React.useState({
+        email: '',
+        password: ''
+    });
+    const changeHandler = e => {
+        setCredentials({...credentials, [e.target.name]: e.target.value})
+    }
     return(
         <div className="container">
             <form className="white"
-                  onSubmit={(e) => {e.preventDefault()}}>
+                  onSubmit={e => {e.preventDefault(); props.signIn(credentials)}}>
                 <h5 className="grey-text text-darken-3">Sign in</h5>
                 <div className="input-field">
                     <label htmlFor="username">Username/Email</label>
-                    <input type="text" id="username"
-                           onChange={(e) => {e.preventDefault()}}/>
+                    <input type="text" id="username" name="email"
+                           onChange={changeHandler}/>
                 </div>
                 <div className="input-field">
                     <label htmlFor="password">Password</label>
-                    <input type="password" id="password"
-                           onChange={(e) => {e.preventDefault()}}/>
+                    <input type="password" id="password" name="password"
+                           onChange={changeHandler}/>
                 </div>
                 <div className="input-field">
                     <button className="btn blue lighten-1 z-depth-0">Sign In</button>
+                    <div className="red-text center">
+                        {props.authError ? <p>{props.authError}</p> : null}
+                    </div>
                 </div>
             </form>
         </div>
---
+++ techquiz/src/containers/SignInPresenter.js
@@ -0,0 +1,19 @@
+import SignIn from "../components/authentication/SignIn";
+import {connect} from "react-redux";
+import {signIn} from "../store/actions/authActions";
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+        signIn: (creds) => dispatch(signIn(creds))
+    }
+}
+
+const mapStateToProps = (state) => {
+    return{
+        authError: state.auth.authError
+    }
+}
+
+const SignInPresenter = connect(mapStateToProps, mapDispatchToProps)(SignIn);
+
+export default SignInPresenter;
