7f17de164497813ff09b03ef24336ca53de5c369
bonnici <bonnici@kth.se>
2021-04-05 16:43:58 +0200

    added authentication reducer

--- techquiz/src/store/reducers/authReducer.js
+++ techquiz/src/store/reducers/authReducer.js
@@ -1,7 +1,33 @@
-const initState = {}
+const initState = {
+    authError: null
+}
 
 const authReducer = (state = initState, action) => {
-    return state
+    switch (action.type){
+        case 'LOGIN_SUCCESS':
+            console.log('login success')
+            return{
+                ...state,
+                authError: null
+            }
+        case 'LOGIN_FAILURE':
+            console.log('login failed')
+            return{
+                ...state,
+                authError: 'Login failed'
+            }
+        case 'SIGNOUT_SUCCESS':
+            console.log('signed out successfully')
+            return state;
+        case 'SIGNOUT_FAILURE':
+            console.log('signed out failure')
+            return{
+                ...state,
+                authError: 'Logout failed'
+            }
+        default:
+            return state;
+    }
 }
 
 export default authReducer
