cd31298bee96a2c7d715e55b614e4567051448f8
Pelle Arn <perarn@kth.se>
2021-04-06 14:58:46 +0200

    added a dashboard presenter

--- techquiz/src/containers/DashboardPresenter.js
+++ techquiz/src/containers/DashboardPresenter.js
@@ -2,7 +2,6 @@ import {connect} from "react-redux";
 import Dashboard from "../components/dashboard/Dashboard";
 
 const mapStateToProps = (state) => {
-    console.log(state, ' that was the state');
     return{
         stats: state.profileStats.stats,
         auth: state.firebase.auth
