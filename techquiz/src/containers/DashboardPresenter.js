1247cad47cfa87a63374546d43d61cc83d1e438f
Pelle Arn <perarn@kth.se>
2021-04-06 14:30:53 +0200

    fixed authtracking for the dashboard

--- techquiz/src/containers/DashboardPresenter.js
+++ techquiz/src/containers/DashboardPresenter.js
@@ -2,6 +2,7 @@ import {connect} from "react-redux";
 import Dashboard from "../components/dashboard/Dashboard";
 
 const mapStateToProps = (state) => {
+    console.log(state, ' that was the state');
     return{
         stats: state.profileStats.stats,
         auth: state.firebase.auth
