73fc7b2d796bb7a6f91d7e33abe5837edeb28321
bonnici <bonnici@kth.se>
2021-04-07 22:02:01 +0200

    Added cloud function that reorders multiplayerRating collection based on rating per user

--- techquiz/functions/index.js
+++ techquiz/functions/index.js
@@ -1,4 +1,8 @@
+
+
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +11,31 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+
+function refactorMlRating() {
+    return admin.firestore()
+        .collection("multiplayerRating")
+        .orderBy('rating', 'desc')
+        .get()
+        .then((querySnapshot) => {
+            let i = 1;
+            querySnapshot.forEach((doc) => {
+                doc.ref.update({
+                    ranking: i
+                }).then(r => console.log(r, "update successful")).catch(error => console.log(error, "wha wha wha"));
+                i++;
+            });
+        }).catch((error) => {
+            console.log(error);
+    });
+}
+
+
+
+exports.mlRatingCreated = functions.firestore
+  .document('multiplayerRating/{userId}')
+  .onCreate((snap,context) => {
+      return refactorMlRating();
+  });
