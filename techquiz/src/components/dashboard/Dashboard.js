1153332bfca5a57f16d4e2ae6cc53ddb07f10945
Pelle Arn <perarn@kth.se>
2021-04-07 12:48:16 +0200

    fixed playerstats ish

--- techquiz/src/components/dashboard/Dashboard.js
+++ techquiz/src/components/dashboard/Dashboard.js
@@ -5,7 +5,15 @@ import FinishedGames from "./FinishedGames";
 import {Redirect} from "react-router-dom";
 
 
-const Dashboard = ({stats, auth}) => {
+const Dashboard = ({stats, auth, stats2}) => {
+    try {
+        const placeholder = auth.uid;
+        console.log(stats2[placeholder]);
+        stats2 = stats2[placeholder];
+        console.log(stats2, 'that was the updated playerstats') }
+    catch {
+        console.log('data hasnt been loaded yet. Will try again soon maybe');
+    }
     if(!auth.uid) {
         return <Redirect to="/signin"/>
     }
