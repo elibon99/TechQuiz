b2faca23d7230f35a6f25fd9d8c9a5508090e713
Pelle Arn <perarn@kth.se>
2021-04-07 11:34:09 +0200

    changed from reg components to presenters as to protect sensitive data from non-logged-in-users

--- techquiz/src/App.js
+++ techquiz/src/App.js
@@ -2,12 +2,12 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import SignUpPresenter from "./containers/SignUpPresenter";
 import FindGamePresenter from "./containers/FindGamePresenter";
 import Leaderboard from "./components/leaderboard/Leaderboard";
-import GameLanding from "./components/game/GameLanding";
-import GameCategory from "./components/game/GameCategory";
-import QuizLanding from "./components/game/QuizLanding";
-import QuizQuestions from "./components/game/QuizQuestions";
-import CurrentGameStats from "./components/game/CurrentGameStats";
-import GameFinished from "./components/game/GameFinished";
+import GameLandingPresenter from "./containers/GameLandingPresenter";
+import GameCategoryPresenter from "./containers/GameCategoryPresenter";
+import QuizLandingPresenter from "./containers/QuizLandingPresenter";
+import QuizQuestionsPresenter from "./containers/QuizQuestionsPresenter";
+import CurrentGameStatsPresenter from "./containers/CurrentGameStatsPresenter";
+import GameFinishedPresenter from "./containers/GameFinishedPresenter";
 import DashboardPresenter from "./containers/DashboardPresenter";
 import NavBarPresenter from "./containers/NavBarPresenter";
 import SignInPresenter from "./containers/SignInPresenter";
@@ -25,12 +25,12 @@ function App() {
               <Route path = '/signin' component={SignInPresenter}></Route>
               <Route path = '/findgame' component={FindGamePresenter}></Route>
               <Route path = '/leaderboard' component={Leaderboard}></Route>
-              <Route path = '/game-landing' component={GameLanding}></Route>
-              <Route path = '/choose-category' component={GameCategory}></Route>
-              <Route path = '/quiz-landing' component={QuizLanding}></Route>
-              <Route path = '/quiz-question' component={QuizQuestions}></Route>
-              <Route path = '/current-game-stats' component={CurrentGameStats}></Route>
-              <Route path = '/game-finished' component={GameFinished}></Route>
+              <Route path = '/game-landing' component={GameLandingPresenter}></Route>
+              <Route path = '/choose-category' component={GameCategoryPresenter}></Route>
+              <Route path = '/quiz-landing' component={QuizLandingPresenter}></Route>
+              <Route path = '/quiz-question' component={QuizQuestionsPresenter}></Route>
+              <Route path = '/current-game-stats' component={CurrentGameStatsPresenter}></Route>
+              <Route path = '/game-finished' component={GameFinishedPresenter}></Route>
               <Route path = '/home' component={LandingPresenter}></Route>
 
           </Switch>
