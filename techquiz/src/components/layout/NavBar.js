3ad2e11ec740214abf20d5c53f4953ae30abc5ba
bonnici <bonnici@kth.se>
2021-04-08 13:24:47 +0200

    Changed path for href

--- techquiz/src/components/layout/NavBar.js
+++ techquiz/src/components/layout/NavBar.js
@@ -9,18 +9,19 @@ const Navbar = (props) => {
     const brandlogo = props.auth.uid ?
         <Link to='/profile' className="brand-logo">TechQuiz</Link>:
         <Link to='/' className="brand-logo">TechQuiz</Link>
+
     return (
         <nav className="wrapper grey darken-3">
             <div className="nav-wrapper container">
                 {brandlogo}
-                <a href="/" className="sidenav-trigger" data-target="mobile-nav">
+                <a href="#" className="sidenav-trigger" data-target="slide-out">
                     <i className="material-icons">menu</i>
                 </a>
 
                 <ul className="right hide-on-med-and-down">
                     {links}
                 </ul>
-                <ul className="sidenav" id="mobile-nav">
+                <ul className="sidenav" id="slide-out">
                     {links}
                 </ul>
             </div>
