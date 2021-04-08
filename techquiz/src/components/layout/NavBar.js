889a61af64bdeee85afc70b21a9edc295b95d87a
bonnici <bonnici@kth.se>
2021-04-08 14:49:21 +0200

    Added useEffect to activate sidenav

--- techquiz/src/components/layout/NavBar.js
+++ techquiz/src/components/layout/NavBar.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Link} from "react-router-dom";
 import LoggedInLinks from "./LoggedInLinks";
 import LoggedOutLinks from './LoggedOutLinks'
+import M from 'materialize-css';
+
 
 
 const Navbar = (props) => {
@@ -10,18 +12,24 @@ const Navbar = (props) => {
         <Link to='/profile' className="brand-logo">TechQuiz</Link>:
         <Link to='/' className="brand-logo">TechQuiz</Link>
 
+    useEffect(() => {
+        var elems = document.querySelectorAll('.sidenav');
+        M.Sidenav.init(elems, {});
+    }, [])
+
+
     return (
         <nav className="wrapper grey darken-3">
             <div className="nav-wrapper container">
                 {brandlogo}
-                <a href="#" className="sidenav-trigger" data-target="slide-out">
+                <a href="/" className="sidenav-trigger" data-target="mobile-demo">
                     <i className="material-icons">menu</i>
                 </a>
 
                 <ul className="right hide-on-med-and-down">
                     {links}
                 </ul>
-                <ul className="sidenav" id="slide-out">
+                <ul className="sidenav" id="mobile-demo">
                     {links}
                 </ul>
             </div>
