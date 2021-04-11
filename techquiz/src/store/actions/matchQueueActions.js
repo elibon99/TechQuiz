dedcc038377365e38d92a4b00efeefae23a2f661
Pelle Arn <perarn@kth.se>
2021-04-11 22:12:54 +0200

    added gameid to matchqueueaction

--- techquiz/src/store/actions/matchQueueActions.js
+++ techquiz/src/store/actions/matchQueueActions.js
@@ -1,17 +1,24 @@
-export const addToMatchQueue = (rating) => {
+export const addToMatchQueue = (rating, username) => {
     return(dispatch, getState, {getFirebase, getFirestore}) =>{
         const firebase = getFirebase();
         const firestore = getFirestore();
-        const uid = firebase.auth.uid;
+        const uid = firebase.auth().currentUser.uid;
 
-        firestore.collection('matchqueue').doc(uid).set({
-            rating: rating,
-        })
-            .then(() => {
-                dispatch({type: 'ADDED_TO_MATCH_QUEUE_SUCCESS'});
+        firestore.collection('users').doc(uid).get()
+            .then((doc) => {firestore.collection('matchqueue').add({
+                uid: uid,
+                rating: rating,
+                username: doc.data().userName,
+                gameID: null
             })
-            .catch((err) => {
-                dispatch({type: 'ADDED_TO_MATCH_QUEUE_FAILURE', err});
-            });
+                .then(() => {
+                    dispatch({type: 'ADDED_TO_MATCH_QUEUE_SUCCESS'});
+                })
+                .catch((err) => {
+                    dispatch({type: 'ADDED_TO_MATCH_QUEUE_FAILURE', err});
+                });
+
+            })
+            .catch((err) => console.log(err));
     }
 }
