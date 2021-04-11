7cd3cdc6d32980fc5ca9726c01f89bab136d15e8
Pelle Arn <perarn@kth.se>
2021-04-11 18:47:55 +0200

    added matchqueuereducer to rootreducer

--- techquiz/src/store/reducers/rootReducer.js
+++ techquiz/src/store/reducers/rootReducer.js
@@ -1,5 +1,6 @@
 import authReducer from "./authReducer";
 import profileStatsReducer from "./profileStatsReducer";
+import matchQueueReducer from "./matchQueueReducer";
 import {combineReducers} from "redux";
 import {firestoreReducer} from "redux-firestore";
 import {firebaseReducer} from "react-redux-firebase";
@@ -7,6 +8,7 @@ import {firebaseReducer} from "react-redux-firebase";
 const rootReducer = combineReducers({
     auth: authReducer,
     profileStats: profileStatsReducer,
+    matchQueue: matchQueueReducer,
     firestore: firestoreReducer,
     firebase: firebaseReducer
 });
