08dafd1595e85e1db46a75811a0436e3ec143f0a
Pelle Arn <perarn@kth.se>
2021-04-11 18:46:50 +0200

    mapped state and dispatch to props

--- techquiz/src/containers/FindGamePresenter.js
+++ techquiz/src/containers/FindGamePresenter.js
@@ -1,12 +1,31 @@
 import {connect} from "react-redux";
 import FindGame from "../components/gameSetup/FindGame";
+import {addToMatchQueue} from "../store/actions/matchQueueActions";
+import {compose} from "redux";
+import {firestoreConnect} from "react-redux-firebase";
 
 const mapStateToProps = (state) => {
+    console.log(state,'state in findgamepres');
+    const uid = state.firebase.auth.uid;
+    const userStats = state.firestore.data.userStats;
+    const userStat = userStats ? userStats[uid] : null;
     return{
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        userStats: userStat,
     }
 }
 
-const FindGamePresenter = connect(mapStateToProps)(FindGame);
+const mapDispatchToProps = (dispatch) => {
+    return{
+        addToQueue: (rating) => dispatch(addToMatchQueue(rating))
+    }
+}
+
+const FindGamePresenter = compose(
+    connect(mapStateToProps, mapDispatchToProps),
+    firestoreConnect([
+        {collection: 'userStats'},
+    ])
+)(FindGame);
 
 export default FindGamePresenter;
