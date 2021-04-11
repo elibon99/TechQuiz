42598aca02b9608c8caf5116cc882b8ca6bb4ff5
Pelle Arn <perarn@kth.se>
2021-04-11 18:46:25 +0200

    tried adding random matchqueue function

--- techquiz/src/components/gameSetup/FindGame.js
+++ techquiz/src/components/gameSetup/FindGame.js
@@ -2,14 +2,14 @@ import React from 'react'
 import QuickMatch from "./QuickMatch";
 import {Redirect} from "react-router-dom";
 
-const FindGame = ({auth}) => {
+const FindGame = ({auth, addToQueue, userStats}) => {
     if(!auth.uid) {
         return <Redirect to="/signin"/>
     }
     return(
             <div className="container find-game-margin">
                 <input placeholder ="Search for player" type="text" id="searchForPlayer"/>
-                <button className="btn blue lighten-1 z-depth-0">Random Match</button>
+                <button disabled={userStats === null} className="btn blue lighten-1 z-depth-0" onClick={userStats ? addToQueue(userStats.mlRating) : null}>Random Match</button>
                 <QuickMatch/>
             </div>
     )
