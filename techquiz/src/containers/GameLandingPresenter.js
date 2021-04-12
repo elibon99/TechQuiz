fd438cc097c97de3389eae733e480829c36ab585
bonnici <bonnici@kth.se>
2021-04-12 20:27:17 +0200

    Mapped games and users collection to gameLanding presenter

--- techquiz/src/containers/GameLandingPresenter.js
+++ techquiz/src/containers/GameLandingPresenter.js
@@ -1,12 +1,33 @@
 import {connect} from "react-redux";
+import {compose} from "redux";
+import {firestoreConnect} from "react-redux-firebase";
 import GameLanding from "../components/game/GameLanding";
 
+
 const mapStateToProps = (state) => {
+    console.log(state)
+    const uid = state.firebase.auth.uid;
+    const users = state.firestore.data.users;
+    const user = users ? users[uid] : null;
+    const gameID = user ? user.currentGameID : null;
+    const games = state.firestore.data.games;
+    const game = (gameID && games) ? games[gameID] : null;
+
     return{
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        game: game
     }
 }
 
-const GameLandingPresenter = connect(mapStateToProps)(GameLanding);
+const GameLandingPresenter = compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        {collection: 'users'},
+        {collection: 'games'}
+    ])
+)(GameLanding);
 
 export default GameLandingPresenter;
+
+
+
