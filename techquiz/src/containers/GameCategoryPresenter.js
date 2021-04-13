86b49537c8afb1031f3e6cd6837215a99875c0ff
Pelle Arn <perarn@kth.se>
2021-04-13 18:49:10 +0200

    copied gamelndingpresenter. it was dope

--- techquiz/src/containers/GameCategoryPresenter.js
+++ techquiz/src/containers/GameCategoryPresenter.js
@@ -1,12 +1,41 @@
 import {connect} from "react-redux";
+import {compose} from "redux";
 import GameCategory from "../components/game/GameCategory";
+import {firestoreConnect} from "react-redux-firebase";
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
+    console.log(state)
+    const id = ownProps.match.params.id;
+    const uid = state.firebase.auth.uid;
+    const games = state.firestore.data.games;
+    const game = (id && games) ? games[id] : null;
+    const userStats = state.firestore.data.userStats;
+    const userStat = userStats ? userStats[uid] : null;
+    const opponentID = game ? (game.userID1 === uid ? game.userID2 : game.userID2) : null;
+    const opponentName = game ? (game.userID1 === uid ? game.user2Name : game.user1Name) : null;
+    const opponentScore = game ? (game.userID1 === uid ? game.p2Score : game.p1Score) : null;
+    const userScore = game ? (game.userID1 === uid ? game.p1Score : game.p2Score) : null;
+    const score = (opponentScore !== null && userScore !== null) ? {userScore: userScore, opponentScore: opponentScore} : null;
+    const opponent = (opponentID && userStats && opponentName) ? {username: opponentName, rating: userStats[opponentID].mlRating} : null;
+    const isYourTurn = game ? (game.turn === uid ? true : false) : null;
+
     return{
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        game: game,
+        opponent: opponent,
+        profile: state.firebase.profile,
+        userStat: userStat,
+        score: score,
+        isYourTurn: isYourTurn
     }
 }
-
-const GameCategoryPresenter = connect(mapStateToProps)(GameCategory);
+const GameCategoryPresenter = compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        {collection: 'users'},
+        {collection: 'games'},
+        {collection: 'userStats'}
+    ])
+)(GameCategory);
 
 export default GameCategoryPresenter;
