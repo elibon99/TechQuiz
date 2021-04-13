aa0101d741ed404ca2b76996f774962a931f5aaa
bonnici <bonnici@kth.se>
2021-04-13 12:45:40 +0200

    Created an action that makes user to redirect to game when pressing random match

--- techquiz/src/store/actions/matchQueueActions.js
+++ techquiz/src/store/actions/matchQueueActions.js
@@ -1,3 +1,4 @@
+
 export const addToMatchQueue = (rating, username) => {
     return(dispatch, getState, {getFirebase, getFirestore}) =>{
         const firebase = getFirebase();
@@ -5,20 +6,46 @@ export const addToMatchQueue = (rating, username) => {
         const uid = firebase.auth().currentUser.uid;
 
         firestore.collection('users').doc(uid).get()
-            .then((doc) => {firestore.collection('matchqueue').add({
-                uid: uid,
-                rating: rating,
-                username: doc.data().userName,
-                gameID: null
-            })
-                .then((doc) => {
-                    dispatch({type: 'ADDED_TO_MATCH_QUEUE_SUCCESS'});
-                })
-                .catch((err) => {
-                    dispatch({type: 'ADDED_TO_MATCH_QUEUE_FAILURE', err});
-                });
+            .then((doc) => {
+                console.log(doc.data(), "user");
+                firestore.collection('matchqueue').get()
+                    .then((querySnapshot) => {
+                        if(querySnapshot.size >= 1){
+                            var gamingSetId = null;
+                            querySnapshot.forEach((doc) => {
+                                if(doc.data().uid !== uid && doc.data().gameID !== null){
+                                    gamingSetId = doc.data().gameID;
+                                }
+                            })
+                        }
+                        else{
+                            gamingSetId = firestore.collection('games').doc().id;
+                        }
+                        firestore.collection('matchqueue').add({
+                            uid: uid,
+                            rating: rating,
+                            username: doc.data().userName,
+                            gameID: gamingSetId
+                        })
+                            .then((docRef) => {
+                                dispatch({type: 'ADDED_TO_MATCH_QUEUE_SUCCESS'});
+                                dispatch({type: 'REDIRECT', payload: `${'/game-landing/' + gamingSetId}`});
+                            })
+                            .catch((err) => {
+                                dispatch({type: 'ADDED_TO_MATCH_QUEUE_FAILURE', err});
+                            });
+
+                    } )
+                    .catch((error) => console.log("could not fetch matchqueue"))
+
 
             })
             .catch((err) => console.log(err));
+    }
+}
+
+export const restoreRedirectTo = () => {
+    return(dispatch) => {
+        dispatch({type: 'RESTORE_REDIRECT_TO'});
     }
 }
