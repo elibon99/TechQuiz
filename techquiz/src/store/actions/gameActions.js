601c53b6bc929621a393677add9ade63e8082b4f
bonnici <bonnici@kth.se>
2021-04-14 11:30:55 +0200

    Fixed action that fetches question from API and adds them in firetore

--- techquiz/src/store/actions/gameActions.js
+++ techquiz/src/store/actions/gameActions.js
@@ -1,7 +1,23 @@
+// const categories = [
+//     "Linux", "DevOps", "MySQL", "PHP", "BASH", "Dockers",
+//     "HTML", "WordPress", "Laravel", "Kubernetes", "JavaScript"
+// ]
+
 const categories = [
-    "Linux", "DevOps", "MySQL", "PHP", "BASH", "Dockers",
-    "HTML", "WordPress", "Laravel", "Kubernetes", "JavaScript"
+    {category: "Linux", value: "Linux", tags: "Linux"},
+    {category: "DevOps", value: "DevOps", tags: "DevOps"},
+    {category: "MySQL", value: "SQL", tags: "MySQL"},
+    {category: "PHP", value: "Code", tags:"PHP"},
+    {category: "BASH", value: "bash", tags: "BASH"},
+    {category: "Docker", value: "Docker", tags: "Docker"},
+    {category: "HTML", value: "Code", tags: "HTML"},
+    {category: "WordPress", value: "CMS", tags: "WordPress"},
+    {category: "Laravel", value: "Code", tags: "Laravel"},
+    {category: "Kubernetes", value: "", tags: "Kubernetes"},
+    {category: "JavaScript", value: "Code", tags: "JavaScript"}
 ]
+
+
 
 export const generateCategories = () => {
     return(dispatch) => {
@@ -20,8 +36,37 @@ function getCategories(categories){
     return selectedCategories;
 }
 
-export const chosenCategory = () => {
-    return(dispatch) => {
-        dispatch({type: 'SELECTED_CATEGORY_SUCCESS'});
+const URL = "https://quizapi.io/api/v1/questions?";
+const API_KEY  = "UwbOCHKVnP7pMftLcMxEB2vvycdJtFg7rOAWaEKR";
+
+export const fetchQuestions = (gamingID, category) => {
+    return(dispatch, getState, {getFirestore}) => {
+        const firestore = getFirestore();
+
+        console.log(category, " choosen category")
+        const params = {tags: category, limit: "3"};
+        const pp = new URLSearchParams(params);
+        const test = URL + pp;
+        console.log(test)
+
+        fetch(URL + new URLSearchParams(params), {
+            "method": "GET",
+            "headers": {
+                "X-API-KEY": API_KEY,
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Accept": "application/json"
+            }
+        }).then(resp => resp.json())
+            .then((resp) => {
+                firestore.collection('games').doc(gamingID).collection('gameSets').add({
+                    questions: {resp},
+                    score: 0,
+                    category: category
+                })
+                    .then((doc) => console.log("Updated games with gameSets questions"))
+                    .catch((error) => console.log("Error updating games: ", error));
+            })
+            .catch((error) => console.log("Failed fetch from API :", error));
+
     }
 }
