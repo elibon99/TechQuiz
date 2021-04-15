d74a78cf6821242195d54df0151178c9008186c8
bonnici <bonnici@kth.se>
2021-04-15 12:16:22 +0200

    Mapped correct question to QuizQuestions

--- techquiz/src/containers/QuizQuestionsPresenter.js
+++ techquiz/src/containers/QuizQuestionsPresenter.js
@@ -1,12 +1,45 @@
 import {connect} from "react-redux";
 import QuizQuestions from "../components/game/QuizQuestions";
+import {compose} from "redux";
+import {firestoreConnect} from "react-redux-firebase";
+import {verifyQuestion} from "../store/actions/gameActions";
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
+    const id = ownProps.match.params.id;
+    const games = state.firestore.data.games;
+    const game = (id && games) ? games[id] : null;
+    const gameSetID = game ? game.currentSet : null;
+    const gameSets = state.firestore.data.Ggamesets;
+    const gameSet = gameSetID ? (gameSets[gameSetID] === undefined ? gameSets : null) : null;
+    // if(gameSet){
+    //     console.log(gameSet, "in quiz question presenter")
+    // }
     return{
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        gameSetID: gameSetID,
+        gameID: id,
+        gameSet: gameSet
     }
 }
 
-const QuizQuestionsPresenter = connect(mapStateToProps)(QuizQuestions);
+const mapDispatchToProps = (dispatch) => {
+    return{
+        verifyQuestion: (gamingID, answer, gameSetID) => dispatch(verifyQuestion(gamingID,answer, gameSetID))
+    }
+}
 
+
+const QuizQuestionsPresenter = compose(
+    connect(mapStateToProps, mapDispatchToProps),
+    firestoreConnect((props) => [
+        {collection: 'games',
+            doc: props.gameID,
+            subcollections: [
+                {collection: 'gameSets', doc: props.gameSetID}
+            ],
+            storeAs: 'Ggamesets'
+        },
+        {collection: 'games'}
+    ])
+)(QuizQuestions);
 export default QuizQuestionsPresenter;
