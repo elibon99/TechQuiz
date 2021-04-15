b202f8ee69d7a193593639e3f0810648e10bc92e
bonnici <bonnici@kth.se>
2021-04-15 12:16:57 +0200

    Display data from game

--- techquiz/src/components/game/QuizLanding.js
+++ techquiz/src/components/game/QuizLanding.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import {Link, Redirect} from "react-router-dom";
 
-const QuizLanding = ({auth, category}) => {
+const QuizLanding = ({auth, gameSet, gameID}) => {
+
+    // if(gameSet){
+    //     console.log(gameSet, "in quiz landing")
+    // }
+
     if(!auth.uid) {
         return <Redirect to="/signin"/>
     }
@@ -11,10 +16,10 @@ const QuizLanding = ({auth, category}) => {
                 <div className="card-content">
                     <div className="container">
                         <div className="row category-display-container">
-                            <h5>{category}</h5>
+                            <h5>{gameSet ? gameSet.category : ""}</h5>
                         </div>
                         <div className="row">
-                            <Link to="/quiz-question">
+                            <Link to={"/quiz-question/" + gameID}>
                                 <button className="btn blue lighten-1 z-depth-0 play-button">Click here to begin quiz</button>
                             </Link>
                         </div>
