d2a9d9e8c2f0ff6846b9c4e1850a978d9a65a4f6
Pelle Arn <perarn@kth.se>
2021-04-17 17:54:02 +0200

    added mapping from games to currentgames both turns, sent to subcomponents

--- techquiz/src/containers/DashboardPresenter.js
+++ techquiz/src/containers/DashboardPresenter.js
@@ -8,21 +8,53 @@ const mapStateToProps = (state) => {
     const userStats = state.firestore.data.userStats;
     const userStat = userStats ? userStats[uid] : null;
     const winLossRatio = userStat ? (userStat.losses !== 0 ? (userStat.wins / userStat.losses): userStat.wins) : "NaN"
+    const games = state.firestore.data.games;
+
+    const gameEntries = games ? Object.entries(games) : null;
+
+    let currentGamesYourTurn = (games && uid) ? gameEntries.filter((entry) => {
+        return (entry[1].userID1 === uid || entry[1].userID2 === uid) && entry[1].turn === uid;
+    }) : null;
+    let currentGamesTheirTurn = (games && uid) ? gameEntries.filter((entry) => {
+        return (entry[1].userID1 === uid || entry[1].userID2 === uid) && entry[1].turn !== uid;
+    }) : null;
+
+    currentGamesYourTurn ? currentGamesYourTurn.forEach((entry) => {
+        if(entry[1].userID1 === uid) {
+            entry.push({opponentName : entry[1].user2Name});
+        }
+        else{
+            entry.push({opponentName : entry[1].user1Name});
+        }
+    }) : console.log('currentGamesYourTurnNONONO');
+
+    currentGamesTheirTurn ? currentGamesTheirTurn.forEach((entry) => {
+        if(entry[1].userID1 === uid) {
+            entry.push({opponentName : entry[1].user2Name});
+        }
+        else {
+            entry.push({opponentName : entry[1].user1Name});
+        }
+    }) : console.log('currentGamesTheirTurnNONONO');
 
     return{
         auth: state.firebase.auth,
         userStat: userStat,
         profile: state.firebase.profile,
-        winLossRatio: winLossRatio
+        winLossRatio: winLossRatio,
+        currentGamesYourTurn: currentGamesYourTurn,
+        currentGamesTheirTurn: currentGamesTheirTurn
     }
 }
 
 const DashboardPresenter = compose(
     connect(mapStateToProps),
-    firestoreConnect([
+    firestoreConnect((props) => [
         {collection: 'users'},
         {collection: 'userStats'},
-        {collection: 'multiplayerRating'}
+        {collection: 'multiplayerRating'},
+        {collection: 'games',
+        }
     ])
     )(Dashboard);
 
