639c3f444598006871999661c05fa71fdb6764d0
Pelle Arn <perarn@kth.se>
2021-04-17 17:53:27 +0200

    added dynamic showing games where its ur turn

--- techquiz/src/components/dashboard/GameItemYourTurn.js
+++ techquiz/src/components/dashboard/GameItemYourTurn.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import {Link} from "react-router-dom";
 
-const GameItemYourTurn = () => {
+const GameItemYourTurn = ({game}) => {
     return(
+        game ?
         <div className="gameitem-container">
                 <div className="gameitem-col">
-                   Per
+                    {game[2].opponentName !== "" ? game[2].opponentName : "Random Player"}
                 </div>
                 <div className="gameitem-col">
-                    <Link to='/game-landing'><button className="right btn waves-effect waves-light #64b5f6 blue lighten-2">Play</button></Link>
+                    <Link to={'/game-landing/' + game[0]}><button className="right btn waves-effect waves-light #64b5f6 blue lighten-2">Play</button></Link>
                 </div>
         </div>
+            : <img src={"http://www.csc.kth.se/~cristi/loading.gif"} alt={"waiting for data"}/>
     )
 }
 
