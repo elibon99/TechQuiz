e4f7e210d3e458e0355835d8357632c53cc0ee5e
Pelle Arn <perarn@kth.se>
2021-04-19 18:42:02 +0200

    added friends redirect to navbar

--- techquiz/src/components/layout/LoggedInLinks.js
+++ techquiz/src/components/layout/LoggedInLinks.js
@@ -6,6 +6,7 @@ const LoggedInLinks = (props) => {
     return(
         <>
             <li><NavLink to='/findgame'>Find a game</NavLink></li>
+            <li><NavLink to='/friends'>Friends</NavLink></li>
             <li><NavLink to='/leaderboard'>Leaderboards</NavLink></li>
             <li><a href="/signin" onClick={props.signOut}>Log out</a></li>
             <li><NavLink to='/profile' className='btn btn-floating blue lighten-1'>{props.profile.initials}</NavLink></li>
