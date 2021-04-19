92a07404ee72a5a01f1946b541c07ff6fa493fbd
bonnici <bonnici@kth.se>
2021-04-19 22:27:25 +0200

    Mapped usernames to input from searchfield dynamically

--- techquiz/src/components/friends/FriendLanding.js
+++ techquiz/src/components/friends/FriendLanding.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {Redirect} from 'react-router-dom';
 import FriendItem from "./FriendItem";
+import FriendRequestItem from "./FriendRequestItem";
+import {Link} from "react-router-dom";
 
-const FriendLanding = ({auth, friendList}) => {
+const FriendLanding = ({auth, friends, friendSearch, setUsername}) => {
+
     if(!auth.uid){
         return <Redirect to="/signin"/>
     }
+
     return (
         <div className="container">
             <div className="row">
@@ -17,22 +21,35 @@ const FriendLanding = ({auth, friendList}) => {
 
                         <div className="card">
                             <div className="card-content">
+                                <FriendRequestItem/>
+                                <FriendRequestItem/>
+                                <FriendRequestItem/>
+                                <FriendRequestItem/>
                             </div>
                         </div>
                     </div>
                 </div>
                     <div className="col s12 m8">
-                        <h5 className="center">
-                            Friends
-                        </h5>
-                        <FriendItem/>
-                        <FriendItem/>
-                        <FriendItem/>
-                        <FriendItem/>
-                        <FriendItem/>
+                        <form className="white"
+                              onChange={(e) => {e.preventDefault();}}>
+                            <h5 className="grey-text text-darken-3">Friends</h5>
+                            <div className="input-field">
+                                <input type="text" id="userName" name="userName" placeholder="Search for friends.."
+                                       onChange={e => {setUsername(e.target.value)}}/>
+                            </div>
+                        </form>
+                        {friends && Object.entries(friends).map((friend) => {
+                            return(
+                                <Link to={'/profile/' + friend[0]} key={friend[0]}>
+                                    <FriendItem friend={friend[1]}/>
+                                </Link>
+
+
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
-        </div>
     )
 }
 
--- techquiz/src/containers/FriendPresenter.js
+++ techquiz/src/containers/FriendPresenter.js
@@ -1,12 +1,36 @@
 import {connect} from "react-redux";
+import {compose} from "redux";
+import {firestoreConnect} from "react-redux-firebase";
 import FriendLanding from "../components/friends/FriendLanding";
+import {setUsername} from "../store/actions/friendActions";
 
 const mapStateToProps = (state) => {
+    const users = state.firestore.data.UsersFriends;
+    const friends = users ? users : null;
     return{
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        friendSearch: state.friends.username,
+        friends: friends
     }
 }
 
-const FriendPresenter = connect(mapStateToProps)(FriendLanding);
+const mapDispatchToProps = (dispatch) => {
+    return{
+        setUsername: (username) => dispatch(setUsername(username))
+    }
+}
+
+const FriendPresenter = compose(
+    connect(mapStateToProps, mapDispatchToProps),
+    firestoreConnect((props) => [
+        {collection: 'users',
+        where: [
+            ['userName', '>=', props.friendSearch],
+            ['userName', '<=', props.friendSearch + '\uf8ff']
+        ],
+            storeAs: 'UsersFriends'
+        }
+    ])
+)(FriendLanding);
 
 export default FriendPresenter;
