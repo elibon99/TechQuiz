03534371116fdcf4e1de9d9e330fa4be25e50886
Pelle Arn <perarn@kth.se>
2021-04-21 21:46:09 +0200

    added accept, reject requests and addfriend

--- techquiz/src/store/actions/friendActions.js
+++ techquiz/src/store/actions/friendActions.js
@@ -5,16 +5,16 @@ export const setUsername = (userName) => {
     }
 }
 
-export const addFriend = (userID) => {
-    return(dispatch, getState, {getFirebase, getFirestore}) => {
+export const addFriend = (userID, otherName) => {
+    return(dispatch, getState, {getFirestore}) => {
         const firestore = getFirestore();
         const self = getState().firebase.auth.uid;
+        const selfName = getState().firebase.profile.userName;
         const otherPerson = userID;
 
-        console.log(self, 'self');
-        console.log(otherPerson, 'otherguy');
-
         firestore.collection('friendRequests').add({
+            sentReqUserName: selfName,
+            gotReqUserName: otherName,
             sentRequest: self,
             gotRequest: otherPerson,
             isAccepted: false,
@@ -27,5 +27,35 @@ export const addFriend = (userID) => {
                 console.log('hello is this happening', err);
                 dispatch({type: 'ADD_FRIEND_FAILURE', err});
             });
+    }
+}
+
+export const acceptFriendRequest = (requestID) => {
+    return(dispatch, getState, {getFirestore}) => {
+        const firestore = getFirestore();
+        firestore.collection('friendRequests').doc(requestID).update({
+            isAccepted: true
+        }).then(() =>{
+            console.log('accepted a friend request');
+            dispatch({type: 'ACCEPTED_REQUEST_SUCCESS'})
+        }).catch((err) => {
+            console.log('hello is this happening', err);
+            dispatch({type: 'ACCEPTED_REQUEST_FAILURE', err});
+        });
+    }
+}
+
+export const rejectFriendRequest = (requestID) => {
+    return(dispatch, getState, {getFirestore}) => {
+        const firestore = getFirestore();
+        firestore.collection('friendRequests').doc(requestID).update({
+            isRejected: true
+        }).then(() =>{
+            console.log('rejected a friend request');
+            dispatch({type: 'REJECTED_REQUEST_SUCCESS'})
+        }).catch((err) => {
+            console.log('hello is this happening', err);
+            dispatch({type: 'REJECTED_REQUEST_FAILURE', err});
+        });
     }
 }
