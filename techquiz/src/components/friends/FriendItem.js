f552b4067cb30e02d5f1bd81de15d7262204872b
Pelle Arn <perarn@kth.se>
2021-04-21 22:12:36 +0200

    acidentally changed prop name so no real difference

--- techquiz/src/components/friends/FriendItem.js
+++ techquiz/src/components/friends/FriendItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const FriendItem = ({friend}) => {
+const FriendItem = ({user}) => {
     return (
         <div className="col s12 m3 player-content">
             <div className="card card-friend-item">
                 <div className="card-content friends-card-padding">
                     <i className="medium material-icons">account_circle</i>
-                    <h6>{friend.userName}</h6>
+                    <h6>{user.userName}</h6>
                 </div>
             </div>
         </div>
