bb9c13ce5e9e0b4e4eef5013353c65159ed20964
Pelle Arn <perarn@kth.se>
2021-04-21 18:08:59 +0200

    added a proflie-friend-preview component-presenter

---
+++ techquiz/src/containers/ProfilePreviewPresenter.js
@@ -0,0 +1,33 @@
+import {connect} from "react-redux";
+import ProfilePreview from "../components/friends/ProfilePreview";
+import {firestoreConnect} from "react-redux-firebase";
+import {compose} from "redux";
+
+const mapStateToProps = (state, ownProps) => {
+    const uid = ownProps.match.params.id;
+    const users = state.firestore.data.users;
+    const user = users ? users[uid] : null;
+    const userName = user ? user.userName : null;
+    const userStats = state.firestore.data.userStats;
+    const userStat = userStats ? userStats[uid] : null;
+    const winLossRatio = userStat ? (userStat.losses !== 0 ? (userStat.wins / userStat.losses): userStat.wins) : "NaN"
+
+    return{
+        auth: state.firebase.auth,
+        userName: userName,
+        userStat: userStat,
+        winLossRatio: winLossRatio,
+    }
+}
+
+const ProfilePreviewPresenter = compose(
+    connect(mapStateToProps),
+    firestoreConnect((props) => [
+        {collection: 'users'},
+        {collection: 'userStats'},
+        {collection: 'multiplayerRating'}
+    ])
+)(ProfilePreview);
+
+export default ProfilePreviewPresenter;
+
