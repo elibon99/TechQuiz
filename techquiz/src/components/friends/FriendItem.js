b4348228f7423496338d34787c10c3e744ac0e76
Pelle Arn <perarn@kth.se>
2021-04-21 19:15:20 +0200

    removed padding from profile cards in friends component, changed css to match

--- techquiz/src/components/friends/FriendItem.js
+++ techquiz/src/components/friends/FriendItem.js
@@ -3,7 +3,7 @@ import React from 'react';
 const FriendItem = ({friend}) => {
     return (
         <div className="col s12 m3 player-content">
-            <div className="card">
+            <div className="card card-friend-item">
                 <div className="card-content friends-card-padding">
                     <i className="medium material-icons">account_circle</i>
                     <h6>{friend.userName}</h6>
