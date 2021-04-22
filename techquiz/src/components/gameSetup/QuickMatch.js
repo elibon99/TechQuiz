6838a9051a4057fe85f186ad2353d6e30182adfc
bonnici <bonnici@kth.se>
2021-04-22 20:03:19 +0200

    Mapping through friends collection based on search and displaying friend game items

--- techquiz/src/components/gameSetup/QuickMatch.js
+++ techquiz/src/components/gameSetup/QuickMatch.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import GameItemYourTurn from "../dashboard/GameItemYourTurn";
+import FriendGameItem from "./FriendGameItem";
+import {Redirect} from "react-router-dom";
 
-const QuickMatch = () => {
+const QuickMatch = ({friends, createFriendGame, friendGameStatus, restoreRedirectTo}) => {
+    if(friendGameStatus.redirectTo){
+        const path = friendGameStatus.redirectTo;
+        restoreRedirectTo();
+        return <Redirect to={path}/>
+    }
     return (
         <div className="card">
             <div className="card-content">
                 <h4 className="quick-match-title">Quick match</h4>
                 <h5>Friends</h5>
-                <GameItemYourTurn/>
+                {friends && Object.entries(friends).map((friend) => {
+                    return <FriendGameItem key={friend[0]} friendInfo={friend[1]} createFriendGame={createFriendGame}/>
+                })}
             </div>
             <div className="card-content">
                 <h5>Recent Players</h5>
