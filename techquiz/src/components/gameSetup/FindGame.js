aafcf1bc6a27d8c52efbe19e1428569e4c0f0d7e
bonnici <bonnici@kth.se>
2021-04-22 20:02:06 +0200

    Passed props to children components

--- techquiz/src/components/gameSetup/FindGame.js
+++ techquiz/src/components/gameSetup/FindGame.js
@@ -2,7 +2,7 @@ import React from 'react'
 import QuickMatch from "./QuickMatch";
 import {Redirect} from 'react-router-dom';
 
-const FindGame = ({auth, addToQueue, restoreRedirectTo, userStats, user, matchQueue}) => {
+const FindGame = ({auth, addToQueue, restoreRedirectTo, userStats, user, matchQueue, createFriendGame, friends, setUsername, restoreRedirectToFriendGame, friendGameStatus}) => {
     if(!auth.uid) {
         return <Redirect to="/signin"/>
     }
@@ -17,9 +17,9 @@ const FindGame = ({auth, addToQueue, restoreRedirectTo, userStats, user, matchQu
     return(
         userStats ?
             <div className="container find-game-margin">
-                <input placeholder ="Search for player" type="text" id="searchForPlayer"/>
+                <input placeholder ="Search for player" type="text" id="searchForPlayer" onChange={e => {setUsername(e.target.value)}}/>
                 <button className="btn blue lighten-1 z-depth-0" onClick={() => {addToQueue(userStats.mlRating)}}>Random Match</button>
-                <QuickMatch/>
+                <QuickMatch friends={friends} createFriendGame={createFriendGame} friendGameStatus={friendGameStatus} restoreRedirectTo={restoreRedirectToFriendGame}/>
             </div> :
                 <img src={"http://www.csc.kth.se/~cristi/loading.gif"} alt={"waiting for data"}/>
     )
