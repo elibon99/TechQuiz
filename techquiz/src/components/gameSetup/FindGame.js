ca6857834bfc1520cd4b0cf7e65505274ac14b92
Pelle Arn <perarn@kth.se>
2021-04-26 14:47:52 +0200

    added friendstemp props

--- techquiz/src/components/gameSetup/FindGame.js
+++ techquiz/src/components/gameSetup/FindGame.js
@@ -2,7 +2,7 @@ import React from 'react'
 import QuickMatch from "./QuickMatch";
 import {Redirect} from 'react-router-dom';
 
-const FindGame = ({auth, addToQueue, restoreRedirectTo, userStats, user, matchQueue, createFriendGame, friends, setUsername, restoreRedirectToFriendGame, friendGameStatus, recentPlayers}) => {
+const FindGame = ({auth, addToQueue, restoreRedirectTo, userStats, user, matchQueue, createFriendGame, friends, setUsername, restoreRedirectToFriendGame, friendGameStatus, recentPlayers, friendsTemp}) => {
     if(!auth.uid) {
         return <Redirect to="/signin"/>
     }
@@ -16,11 +16,12 @@ const FindGame = ({auth, addToQueue, restoreRedirectTo, userStats, user, matchQu
 
     return(
         userStats ?
-            <div className="container find-game-margin">
-                <input placeholder ="Search for player" type="text" id="searchForPlayer" onChange={e => {setUsername(e.target.value)}}/>
-                <button className="btn blue lighten-1 z-depth-0" onClick={() => {addToQueue(userStats.mlRating)}}>Random Match</button>
-                <QuickMatch recentPlayers={recentPlayers} friends={friends} createFriendGame={createFriendGame} friendGameStatus={friendGameStatus} restoreRedirectTo={restoreRedirectToFriendGame}/>
-            </div> :
+            <div><h5 className="page-title">Find a game</h5>
+                <div className="container find-game-margin">
+                    <button className="btn blue lighten-1 z-depth-0" onClick={() => {addToQueue(userStats.mlRating)}}>Random Match</button>
+                    <QuickMatch friendsTemp={friendsTemp} setUsername={setUsername} recentPlayers={recentPlayers} friends={friends} createFriendGame={createFriendGame} friendGameStatus={friendGameStatus} restoreRedirectTo={restoreRedirectToFriendGame}/>
+                </div>
+            </div>:
                 <img src={"http://www.csc.kth.se/~cristi/loading.gif"} alt={"waiting for data"}/>
     )
 }
