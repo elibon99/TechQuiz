8d33bd4137934324a7e6f3a2a316de60e518ad25
Pelle Arn <perarn@kth.se>
2021-04-26 14:48:27 +0200

    fixed some conditional rendering when searching for friends, with having friends, having no friends etc

--- techquiz/src/components/gameSetup/QuickMatch.js
+++ techquiz/src/components/gameSetup/QuickMatch.js
@@ -1,28 +1,38 @@
 import React from 'react'
 import FriendGameItem from "./FriendGameItem";
 import RecentPlayerGameItem from "./RecentPlayerGameItem";
-import {Redirect} from "react-router-dom";
+import {Link, Redirect} from "react-router-dom";
 
-const QuickMatch = ({friends, createFriendGame, friendGameStatus, restoreRedirectTo, recentPlayers}) => {
+const QuickMatch = ({friends, createFriendGame, friendGameStatus, restoreRedirectTo, recentPlayers, setUsername, friendsTemp}) => {
+    console.log(recentPlayers.length, 'recenteplayersoiasoinsoin');
     if(friendGameStatus.redirectTo){
         const path = friendGameStatus.redirectTo;
         restoreRedirectTo();
         return <Redirect to={path}/>
     }
     return (
-        <div className="card">
-            <div className="card-content">
-                <h4 className="quick-match-title">Quick match</h4>
-                <h5>Friends</h5>
-                {friends && Object.entries(friends).map((friend) => {
-                    return <FriendGameItem key={friend[0]} friendInfo={friend[1]} createFriendGame={createFriendGame}/>
-                })}
+        <div>
+            <div className="card card-quick-match">
+                <div className="card-content">
+                    <h5>Friends</h5>
+                    {friendsTemp ? <input placeholder ="Search for player" type="text" id="searchForPlayer" onChange={e => {setUsername(e.target.value)}}/> : null}
+                    {friends ?
+                        Object.entries(friends).map((friend) => {
+                            return <FriendGameItem key={friend[0]} friendInfo={friend[1]} createFriendGame={createFriendGame}/>
+                        }) : friendsTemp === null ? <h6>You haven't added any friends. Go to the <Link to= "/friends"><button className="btn waves-effect waves-light #64b5f6 blue lighten-2"> friends </button></Link> page and make some friends!</h6>
+                            : <div>Couldn't find this player</div> }
+                </div>
             </div>
-            <div className="card-content">
-                <h5>Recent Players</h5>
-                {recentPlayers && recentPlayers.map((player) => {
-                    return <RecentPlayerGameItem key={player.opponentID} opponentInfo={player}/>
-                })}
+            <div className="card card-quick-match">
+                <div className="card-content">
+                    <h5>Recent Players</h5>
+                    {recentPlayers.length !== 0 ? recentPlayers.map((player) => {
+                            return <RecentPlayerGameItem key={player.opponentID} opponentInfo={player}/>
+                        }) :
+                        <h6>You don't have any recent players. Press the <span className="text-title-recentplayers">Random Match</span> button at the top of this page to find a game against a random opponent!</h6>
+
+                    }
+                </div>
             </div>
         </div>
     )
