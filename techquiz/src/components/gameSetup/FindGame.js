c64993f8885c481dc7a40aeeaf0dd5aec7ed0968
bonnici <bonnici@kth.se>
2021-04-26 12:12:36 +0200

    Mapped recentplayers to FindGame component

--- techquiz/src/components/gameSetup/FindGame.js
+++ techquiz/src/components/gameSetup/FindGame.js
@@ -2,7 +2,7 @@ import React from 'react'
 import QuickMatch from "./QuickMatch";
 import {Redirect} from 'react-router-dom';
 
-const FindGame = ({auth, addToQueue, restoreRedirectTo, userStats, user, matchQueue, createFriendGame, friends, setUsername, restoreRedirectToFriendGame, friendGameStatus}) => {
+const FindGame = ({auth, addToQueue, restoreRedirectTo, userStats, user, matchQueue, createFriendGame, friends, setUsername, restoreRedirectToFriendGame, friendGameStatus, recentPlayers}) => {
     if(!auth.uid) {
         return <Redirect to="/signin"/>
     }
@@ -19,7 +19,7 @@ const FindGame = ({auth, addToQueue, restoreRedirectTo, userStats, user, matchQu
             <div className="container find-game-margin">
                 <input placeholder ="Search for player" type="text" id="searchForPlayer" onChange={e => {setUsername(e.target.value)}}/>
                 <button className="btn blue lighten-1 z-depth-0" onClick={() => {addToQueue(userStats.mlRating)}}>Random Match</button>
-                <QuickMatch friends={friends} createFriendGame={createFriendGame} friendGameStatus={friendGameStatus} restoreRedirectTo={restoreRedirectToFriendGame}/>
+                <QuickMatch recentPlayers={recentPlayers} friends={friends} createFriendGame={createFriendGame} friendGameStatus={friendGameStatus} restoreRedirectTo={restoreRedirectToFriendGame}/>
             </div> :
                 <img src={"http://www.csc.kth.se/~cristi/loading.gif"} alt={"waiting for data"}/>
     )
