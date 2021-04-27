555128ce792b6751851eac70a496da0c6d0772a4
bonnici <bonnici@kth.se>
2021-04-27 14:24:49 +0200

    Added some css styling

--- techquiz/src/components/dashboard/CurrentGames.js
+++ techquiz/src/components/dashboard/CurrentGames.js
@@ -7,7 +7,7 @@ const CurrentGames = ({currentGamesYourTurn, currentGamesTheirTurn}) => {
         <div className="dashboard-item-container container section">
             <h5>Current Games:</h5>
             <div className="row">
-                <div className="col s12 m5">
+                <div className="col s12 m5 profile-current-games-col">
                     Your turn:
                     {currentGamesYourTurn && currentGamesYourTurn.map((entry) => {
                         return (
@@ -16,7 +16,7 @@ const CurrentGames = ({currentGamesYourTurn, currentGamesTheirTurn}) => {
                     })}
 
                 </div>
-                <div className="col s12 m5 offset-m2">
+                <div className="col s12 m5 offset-m2 profile-current-games-col">
                     Their turn:
                     {currentGamesTheirTurn && currentGamesTheirTurn.map((entry) => {
                         return(
