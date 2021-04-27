d937cdc61d326e1454eab761eaabf0e760d21eff
Pelle Arn <perarn@kth.se>
2021-04-27 15:55:57 +0200

    fixed link to profile of friendreq

--- techquiz/src/components/friends/FriendRequestItem.js
+++ techquiz/src/components/friends/FriendRequestItem.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import ThumbsDown from "./ThumbsDown";
 import ThumbsUp from "./ThumbsUp";
+import {Link} from "react-router-dom";
 
 const FriendRequestItem = ({request, requestID, rejectFriendRequest, acceptFriendRequest}) => {
     return(
-        <div className="friend-req-container">
-            <div>
-                {request.sentReqUserName}
+        <div>
+            <Link to={"/profile-preview/" + request.sentRequest}>
+            <div className="friend-req-container link-to-profile-friend-req">
+                    <div>
+                        {request.sentReqUserName}
+                    </div>
+                <div className="thumbs-container">
+                    <button onClick={() => acceptFriendRequest(requestID)}>
+                        <ThumbsUp className="thumbs-up"/>
+                    </button>
+                    <button onClick={() => rejectFriendRequest(requestID)}>
+                        <ThumbsDown className="thumbs-down"/>
+                    </button>
+                </div>
             </div>
-
-            <div className="thumbs-container">
-                <button onClick={() => acceptFriendRequest(requestID)}>
-                    <ThumbsUp className="thumbs-up"/>
-                </button>
-                <button onClick={() => rejectFriendRequest(requestID)}>
-                    <ThumbsDown className="thumbs-down"/>
-                </button>
-            </div>
+            </Link>
         </div>
     )
 }
