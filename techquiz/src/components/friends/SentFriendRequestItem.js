d28f15747b19a543d916d8f4ba9c764aa57063eb
Pelle Arn <perarn@kth.se>
2021-04-27 15:56:37 +0200

    added a component for showing sent friend reqs that are pending

---
+++ techquiz/src/components/friends/SentFriendRequestItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+const SentFriendRequestItem = ({request}) => {
+    return(
+        <div>
+            <Link to={"/profile-preview/" + request.gotRequest}>
+                <div className="friend-req-container link-to-profile-friend-req">
+                    <div>
+                        {request.gotReqUserName}
+                    </div>
+              </div>
+            </Link>
+        </div>
+    )
+}
+
+export default SentFriendRequestItem;
