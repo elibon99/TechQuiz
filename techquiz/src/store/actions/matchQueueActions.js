fcd4d778b7ee97b687137a9b6a9f62f4fde8fb91
bonnici <bonnici@kth.se>
2021-04-28 11:00:37 +0200

    Created random match game entry from client side instead of in cloud functions

--- techquiz/src/store/actions/matchQueueActions.js
+++ techquiz/src/store/actions/matchQueueActions.js
@@ -1,41 +1,60 @@
 
-export const addToMatchQueue = (rating, username) => {
+export const addToMatchQueue = (rating) => {
     return(dispatch, getState, {getFirebase, getFirestore}) =>{
         const firebase = getFirebase();
         const firestore = getFirestore();
         const uid = firebase.auth().currentUser.uid;
+        const username = getState().firebase.profile.userName;
         let shouldSkip = false;
+        var gamingSetId = null;
+        var shouldCreateNewGame = false;
 
         firestore.collection('users').doc(uid).get()
             .then((doc) => {
-                console.log(doc.data(), "user");
                 firestore.collection('matchqueue').get()
                     .then((querySnapshot) => {
                         if(querySnapshot.size >= 1){
-                            var gamingSetId = null;
                             querySnapshot.forEach((doc) => {
                                 if(shouldSkip){
                                     return;
                                 }
                                 else if(doc.data().uid !== uid && doc.data().gameID !== null){
                                     gamingSetId = doc.data().gameID;
-                                    console.log("First else if happened with gameID: ", gamingSetId)
                                     shouldSkip = true;
                                 }
                                 else if (doc.data().uid === uid){
-                                    console.log('> 1 entry, same userID');
-                                    console.log("Second else if happened")
                                     gamingSetId = firestore.collection('games').doc().id;
+                                    shouldCreateNewGame = true;
                                     shouldSkip = true;
                                 }
                                 else{
-                                    console.log('watafak');
+                                    console.log('Something wrong happenend when creating game');
                                 }
                             })
                         }
                         else{
                             gamingSetId = firestore.collection('games').doc().id;
+                            shouldCreateNewGame = true;
                             console.log("Else statement happened")
+                        }
+                        /* Creating new game either if matchqueue is empty or if entries in matchqueue are mapped to same user */
+                        if(shouldCreateNewGame){
+                            firestore.collection('games').doc(gamingSetId).set({
+                                userID1: uid,
+                                user1Name: username,
+                                userID2: '',
+                                user2Name: '',
+                                turn: uid,
+                                p1Score: 0,
+                                p2Score: 0,
+                                currentSet: "",
+                                shouldCreateNewGameSet : uid,
+                                amountOfPlayerLeft : 2,
+                                redirectTo: null,
+                                gameIsFinished: false,
+                                timeOfGameFinished: null
+                            }).then(() => console.log("Succesfully created game"))
+                                .catch((err) => console.log("Failed to create game :", err));
                         }
                         firestore.collection('matchqueue').add({
                             uid: uid,
@@ -54,8 +73,6 @@ export const addToMatchQueue = (rating, username) => {
 
                     } )
                     .catch((error) => console.log("could not fetch matchqueue"))
-
-
             })
             .catch((err) => console.log(err));
     }
