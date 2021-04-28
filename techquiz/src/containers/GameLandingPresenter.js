5031ecac1c50cef61775824b43dfd22e842ab88a
bonnici <bonnici@kth.se>
2021-04-28 10:59:51 +0200

    Cleaned up code and added comments

--- techquiz/src/containers/GameLandingPresenter.js
+++ techquiz/src/containers/GameLandingPresenter.js
@@ -6,23 +6,33 @@ import {generateCategories, restoreRedirectFirebase} from "../store/actions/game
 
 
 const mapStateToProps = (state, ownProps) => {
-    //console.log(state, "State withinh game landing presenter")
-    const id = ownProps.match.params.id;
-    const uid = state.firebase.auth.uid;
+    /* Getting game data from firestore */
+    const gameID = ownProps.match.params.id;
     const games = state.firestore.data.games;
-    const game = (id && games) ? games[id] : null;
+    const game = (gameID && games) ? games[gameID] : null;
+
+    /* Getting user data from game and userstats */
+    const uid = state.firebase.auth.uid;
     const userStats = state.firestore.data.userStats;
-    const redirectTo = game ? game.redirectTo : null;
     const userStat = userStats ? userStats[uid] : null;
+    const userScore = game ? (game.userID1 === uid ? game.p1Score : game.p2Score) : null;
+
+    /* Getting opponent data from game */
     const opponentID = game ? (game.userID1 === uid ? game.userID2 : game.userID1) : null;
     const opponentName = game ? (game.userID1 === uid ? game.user2Name : game.user1Name) : null;
     const opponentScore = game ? (game.userID1 === uid ? game.p2Score : game.p1Score) : null;
-    const userScore = game ? (game.userID1 === uid ? game.p1Score : game.p2Score) : null;
-    const score = (opponentScore !== null && userScore !== null) ? {userScore: userScore, opponentScore: opponentScore} : null;
     const opponent = (opponentID && userStats && opponentName) ? {username: opponentName, rating: userStats[opponentID].mlRating} : null;
+
+
+    /* Getting current game score */
+    const gameScore = (opponentScore !== null && userScore !== null) ? {userScore: userScore, opponentScore: opponentScore} : null;
+    /* Getting redirectTo attribute for when game is finished */
+    const redirectTo = game ? game.redirectTo : null;
+
+    /* isYourTurn is true if turn is equal to the userID of the currently logged in user, else false*/
     const isYourTurn = game ? (game.turn === uid ? true : false) : null;
+    /* shouldCreateNewGameSet is true if shouldCreateNewGameSet is equal to the userID of the currently logged in user, else false*/
     const shouldCreateNewGameSet = game ? (game.shouldCreateNewGameSet === uid ? true : false) : null;
-    //game ? console.log(game.shouldCreateNewGameSet, ' who is this guy') : console.log('woops');
 
     return{
         auth: state.firebase.auth,
@@ -30,9 +40,9 @@ const mapStateToProps = (state, ownProps) => {
         opponent: opponent,
         profile: state.firebase.profile,
         userStat: userStat,
-        score: score,
+        score: gameScore,
         isYourTurn: isYourTurn,
-        gameID: id,
+        gameID: gameID,
         shouldCreateNewGameSet: shouldCreateNewGameSet,
         redirectTo: redirectTo
     }
