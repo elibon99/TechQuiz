4487fc20f75a393030fd09345e02759ef20f2649
Pelle Arn <perarn@kth.se>
2021-04-29 12:13:53 +0200

    fixed rendering no-players-msg if no current games

--- techquiz/src/components/dashboard/CurrentGames.js
+++ techquiz/src/components/dashboard/CurrentGames.js
@@ -8,26 +8,27 @@ import GameItemTheirTurn from "./GameItemTheirTurn";
  * @param currentGamesTheirTurn - all the active games of the current user where its NOT the users turn,
  * */
 const CurrentGames = ({currentGamesYourTurn, currentGamesTheirTurn}) => {
+    console.log(currentGamesYourTurn, 'cgurturn');
     return(
         <div className="dashboard-item-container container section">
             <h5>Current Games</h5>
             <div className="row">
                 <div className="col s12 m5 profile-current-games-col">
                     Your turn:
-                    {currentGamesYourTurn && currentGamesYourTurn.map((entry) => {
+                    {currentGamesYourTurn ? currentGamesYourTurn.map((entry) => {
                         return (
                                 <GameItemYourTurn game={entry} key={entry[0]}/>
                             )
-                    })}
+                    }) : <div> You don't have any games where it's your turn. Come back later or start a new game here! </div>}
 
                 </div>
                 <div className="col s12 m5 offset-m2 profile-current-games-col">
                     Their turn:
-                    {currentGamesTheirTurn && currentGamesTheirTurn.map((entry) => {
+                    {currentGamesTheirTurn ? currentGamesTheirTurn.map((entry) => {
                         return(
                             <GameItemTheirTurn game={entry} key={entry[0]}/>
                         )
-                    })
+                    }) : <div> You don't have any games where it's your opponents turn. </div>
                     }
                 </div>
             </div>
