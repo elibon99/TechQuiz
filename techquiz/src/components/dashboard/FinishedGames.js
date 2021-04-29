fd921499450285f3ed6ccca3339617422f2bf5b4
Pelle Arn <perarn@kth.se>
2021-04-29 12:14:06 +0200

    fixed rendering no-players-msg if no finished games

--- techquiz/src/components/dashboard/FinishedGames.js
+++ techquiz/src/components/dashboard/FinishedGames.js
@@ -12,11 +12,11 @@ const FinishedGames = ({finishedGames}) => {
             <h5>Finished games</h5>
             <div className="row">
                 <div className="col s12 m12">
-                    {finishedGames && finishedGames.map((entry) => {
+                    {finishedGames ? finishedGames.map((entry) => {
                         return (
                             <GameItemFinished game={entry} key={entry[0]}/>
                         )
-                    })}
+                    }) : <div> You don't have any finished games. Go play some games! </div>}
                 </div>
             </div>
         </div>
