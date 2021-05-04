9efe1439fac59249d5231334bec74c1233af8dc1
bonnici <bonnici@kth.se>
2021-05-04 17:35:20 +0200

    Added logic for displaying in set details in GameSetItem component

--- techquiz/src/components/game/GameSetItem.js
+++ techquiz/src/components/game/GameSetItem.js
@@ -1,24 +1,48 @@
 import React from 'react';
+import Square from "./Square";
 
-const GameSetItem = ({isYourTurn}) => {
-    // if(isYourTurn){
-    //     console.log("It is your turn")
-    // }else{
-    //     console.log("It is not your turn")
-    // }
+
+/**
+ * This components main focus is displaying game-set items, rendering if it's your turn, opponents turn or your and the opponents results
+ * @param isYourTurn - a variable that is set to true if it is your turn,
+ * @param myResults - my set results for a given set,
+ * @param opponentResults - opponent results for a given set
+ * @param category - game set category for a given set
+ * @param currentSet - the current game set of the game
+ * @param setNr - the game set number
+ * @param finishedGames - all finished games of the current user,
+ * @param hasBeenAnsweredBy - a variable that tells how many have answered the given game set
+ * */
+const GameSetItem = ({isYourTurn, myResults, opponentResults, category, currentSet, setNr, hasBeenAnsweredBy}) => {
+
     return(
-        <div className="card-content">
+        <div className="card-content game-set-container">
             <div className="container">
-                <div className="card">
+                <h6 className={"game-set-item-title"}>{setNr}</h6>
+                <div className="card game-set-card-container">
                 <div className="row flex game-item">
                       <div className="col s12 m5 game-set-item">
-                          {isYourTurn ? "Your turn" : ""}
+                          {isYourTurn && myResults===undefined && (setNr === parseInt(currentSet) || hasBeenAnsweredBy[setNr-2] === 2) ? "Your Turn":
+                               myResults && myResults["qScore2"] !== undefined ?
+                                   <div className="set-results-container">
+                                       {Object.entries(myResults).map((results, index) => {
+                                           return <Square key={index} result={results[1]}/>
+                                       })}
+                                   </div> :
+                               setNr > parseInt(currentSet) ? "TBD" : ""}
                       </div>
                       <div className="col s12 m2 game-set-item">
-                                Linux
+                          {category ? category : ""}
                       </div>
                       <div className="col s12 m5 game-set-item">
-                          {!isYourTurn ? "Playing" : ""}
+                          {!isYourTurn && opponentResults===undefined && (setNr === parseInt(currentSet) || hasBeenAnsweredBy[setNr-2] === 2) ? "Playing":
+                           opponentResults &&  opponentResults["qScore2"]!==undefined ?
+                               <div className="set-results-container">
+                                   {Object.entries(opponentResults).map((results, index) => {
+                                       return <Square key={index} result={results[1]}/>
+                                   })}
+                               </div> :
+                           setNr > parseInt(currentSet) ? "TBD" : ""}
                       </div>
                 </div>
                 </div>
