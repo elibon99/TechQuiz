062626f68a771db89628957ba443960de328faac
Pelle Arn <perarn@kth.se>
2021-05-05 20:14:12 +0200

    added friends profile pic + a small placeholder bio

--- techquiz/src/components/friends/ProfilePreview.js
+++ techquiz/src/components/friends/ProfilePreview.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PlayerStats from "../dashboard/PlayerStats";
 import {Redirect} from "react-router-dom";
 
-const ProfilePreview = ({auth, userStat, winLossRatio, userName, addFriend, userID, isFriend, isPending, removeFriend, friendEntryID}) => {
+const ProfilePreview = ({friendPicURL, auth, userStat, winLossRatio, userName, addFriend, userID, isFriend, isPending, removeFriend, friendEntryID}) => {
 
     if(!auth.uid) {
         return <Redirect to="/signin"/>
@@ -11,6 +11,7 @@ const ProfilePreview = ({auth, userStat, winLossRatio, userName, addFriend, user
         (userStat && userName) ?
             <div className="dashboard container">
                 <div className="profile-preview-top">
+                    <img className="friend-profile-pic" src={friendPicURL} alt="friend-profile-pic"/>
                     <h1 className="profile-preview-userName">{userName}</h1>
                     {isFriend ?
                         <div>
@@ -24,6 +25,7 @@ const ProfilePreview = ({auth, userStat, winLossRatio, userName, addFriend, user
                     }
                 </div>
                 <div className="row">
+                    Placeholder BIO I am the tech quiz god
                     <div className="dashboard-item col s12 m12">
                         <PlayerStats stats={userStat} winLossRatio={winLossRatio}/>
                     </div>
