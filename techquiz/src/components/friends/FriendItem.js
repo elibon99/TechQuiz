52467279bb7d255f49d541fe6b46da4fd8f16078
Pelle Arn <perarn@kth.se>
2021-05-05 20:23:04 +0200

    added showing friends profile pic. kinda working

--- techquiz/src/components/friends/FriendItem.js
+++ techquiz/src/components/friends/FriendItem.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const FriendItem = ({user}) => {
+    console.log(user, 'user inside friend');
     return (
         <div className="col s12 m3 player-content">
             <div className="card card-friend-item">
                 <div className="card-content friends-card-padding">
-                    <i className="medium material-icons">account_circle</i>
+                    {user.photoURL ? <img className="profile-pic-friend-frienditem" src={user.photoURL}/>
+                    : <i className="medium material-icons">account_circle</i>}
                     <h6>{user.userName}</h6>
                 </div>
             </div>
