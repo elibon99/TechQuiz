0408d69238299eeb6af8b2afcbac9ac990e39fc3
Pelle Arn <perarn@kth.se>
2021-05-05 20:28:47 +0200

    added an alt tag for the img

--- techquiz/src/components/friends/FriendItem.js
+++ techquiz/src/components/friends/FriendItem.js
@@ -6,7 +6,7 @@ const FriendItem = ({user}) => {
         <div className="col s12 m3 player-content">
             <div className="card card-friend-item">
                 <div className="card-content friends-card-padding">
-                    {user.photoURL ? <img className="profile-pic-friend-frienditem" src={user.photoURL}/>
+                    {user.photoURL ? <img className="profile-pic-friend-frienditem" alt="profile-pic" src={user.photoURL}/>
                     : <i className="medium material-icons">account_circle</i>}
                     <h6>{user.userName}</h6>
                 </div>
