96cf16e7752b80523553b87fc3bf115d2c6c5d3a
bonnici <bonnici@kth.se>
2021-05-06 12:01:39 +0200

    Changed structure of component

--- techquiz/src/components/friends/ProfilePreview.js
+++ techquiz/src/components/friends/ProfilePreview.js
@@ -1,28 +1,49 @@
 import React from 'react';
 import PlayerStats from "../dashboard/PlayerStats";
 import {Redirect} from "react-router-dom";
+import ThumbsUp from "./ThumbsUp";
+import ThumbsDown from "./ThumbsDown";
 
-const ProfilePreview = ({friendPicURL, auth, userStat, winLossRatio, userName, addFriend, userID, isFriend, isPending, removeFriend, friendEntryID}) => {
+const ProfilePreview = ({friendPicURL, auth, userStat, winLossRatio, userName, addFriend, userID, isFriend, isPending, removeFriend, friendEntryID, hasSentMeRequest, requestID, acceptFriendRequest, rejectFriendRequest}) => {
     if(!auth.uid) {
         return <Redirect to="/signin"/>
     }
+
+    console.log(hasSentMeRequest, "has sent me request")
     return (
         (userStat && userName) ?
             <div className="dashboard container">
                 <div className="profile-preview-top">
-                    {friendPicURL ? <img className="friend-profile-pic" alt="friend-profile-pic" src={friendPicURL}/>
-                        : <i className="large material-icons">account_circle</i>}
-                    <h1 className="profile-preview-userName">{userName}</h1>
-                    {isFriend ?
-                        <div>
-                            <h5>You are friends</h5>
-                            <button onClick={() => {removeFriend(userID)}}>Remove friend</button>
-                        </div>: isPending ?
-                        <h5>Pending request</h5> :
-                        <button onClick={() => {addFriend(userID, userName)}}>
-                            Add friend :)
-                        </button>
-                    }
+                    <div className="profile-preview-logo-title-container">
+                        <div className="profile-preview-title-friend-status-container">
+                            <h1 className="profile-preview-userName">{userName}</h1>
+                            {isFriend ?
+                                <div>
+                                    <h5>You are friends</h5>
+                                    <button className="btn waves-effect waves-light red lighten-2" onClick={() => {removeFriend(userID)}}>Remove friend</button>
+                                </div>: isPending ?
+                                    <h5>Pending request...</h5> :
+                                    hasSentMeRequest ?
+                                        <div className="profile-preview-friend-req-container">
+                                            <h6>{userName} has sent you a friend request</h6>
+                                            <div>
+                                                <button className="btn waves-effect waves-light #64b5f6 blue lighten-2 btn-full-height" onClick={() => acceptFriendRequest(requestID)}>Accept</button>
+                                                <button className="btn waves-effect waves-light red lighten-2 btn-full-height" onClick={() => rejectFriendRequest(requestID)}>Reject</button>
+                                            </div>
+                                        </div> :
+                                        <button className="btn waves-effect waves-light #64b5f6 blue lighten-2" onClick={() => {addFriend(userID, userName)}}>
+                                            Add friend :)
+                                        </button>
+                            }
+
+                        </div>
+
+                        {friendPicURL ? <img className="friend-profile-pic" alt="friend-profile-pic" src={friendPicURL}/>
+                            : <i className="large material-icons">account_circle</i>}
+                    </div>
+
+
+
                 </div>
                 <div className="row">
                     Placeholder BIO I am the tech quiz god
