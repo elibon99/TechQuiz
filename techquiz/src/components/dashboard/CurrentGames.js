99ad0d4fcae28b6d820960f1450cc9ab70ef6715
bonnici <bonnici@kth.se>
2021-05-07 15:29:21 +0200

    Changed layout of current games component

--- techquiz/src/components/dashboard/CurrentGames.js
+++ techquiz/src/components/dashboard/CurrentGames.js
@@ -9,20 +9,16 @@ import {Link} from "react-router-dom";
  * @param currentGamesTheirTurn - all the active games of the current user where its NOT the users turn,
  * */
 const CurrentGames = ({currentGamesYourTurn, currentGamesTheirTurn}) => {
-    console.log(currentGamesYourTurn, 'cgurturn');
     return(
-        <div className="dashboard-item-container">
-            <div className="row current-games-container">
-                <h6 className="h6-title-current-games">Your turn:</h6>
-                <div className="col s12 m12 profile-current-games-col-your-turn">
+        <div className="card profile-info-card">
+            <div className="card-content profile-info-card-content">
+                <h5 className="profile-info-card-title">Active games</h5>
+                <div className="profile-info-active-games-container">
                     {currentGamesYourTurn ? currentGamesYourTurn.map((entry) => {
                         return (
                                 <GameItemYourTurn game={entry} key={entry[0]}/>
                             )
                     }) : <h6> You don't have any games where it's your turn. Come back later or start a new game <Link to= "/findgame"><button className="btn waves-effect waves-light #64b5f6 blue lighten-2"> here </button></Link></h6>}
-                </div>
-                <h6 className="h6-title-current-games">Their turn: </h6>
-                <div className="col s12 m12 profile-current-games-col-their-turn">
                     {currentGamesTheirTurn ? currentGamesTheirTurn.map((entry) => {
                         return(
                             <GameItemTheirTurn game={entry} key={entry[0]}/>
