553ab1e20de3c6d6c2f747c2eed9ac5c2ec93820
bonnici <bonnici@kth.se>
2021-05-07 15:29:02 +0200

    Changed layout of SignUp component

--- techquiz/src/components/authentication/SignUp.js
+++ techquiz/src/components/authentication/SignUp.js
@@ -20,32 +20,37 @@ const SignUp = ({signUp, auth, authError}) => {
     if(auth.uid) return <Redirect to='/profile' />
 
     return(
-        <div className="container">
-            <form className="white"
-                  onSubmit={(e) => {e.preventDefault(); signUp(credentials)}}>
-                <h5 className="grey-text text-darken-3">Sign up</h5>
-                <div className="input-field">
-                    <label htmlFor="username">Username</label>
-                    <input type="text" id="userName" name="userName"
-                           onChange={changeHandler}/>
+        <div className="container general-container">
+            <div className="card general-card">
+                <div className="card-content">
+                    <form className="white"
+                          onSubmit={(e) => {e.preventDefault(); signUp(credentials)}}>
+                        <h5 className="grey-text text-darken-3">Sign up</h5>
+                        <div className="input-field">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" id="userName" name="userName"
+                                   onChange={changeHandler}/>
+                        </div>
+                        <div className="input-field">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" id="email" name="email"
+                                   onChange={changeHandler}/>
+                        </div>
+                        <div className="input-field">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" name="password"
+                                   onChange={changeHandler}/>
+                        </div>
+                        <div className="input-field">
+                            <button className="btn blue lighten-1 z-depth-0">Sign Up</button>
+                            <div className="red-text center">
+                                {authError ? <p>{authError}</p> : null}
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <div className="input-field">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email"
-                           onChange={changeHandler}/>
-                </div>
-                <div className="input-field">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password"
-                           onChange={changeHandler}/>
-                </div>
-                <div className="input-field">
-                    <button className="btn blue lighten-1 z-depth-0">Sign Up</button>
-                    <div className="red-text center">
-                        {authError ? <p>{authError}</p> : null}
-                    </div>
-                </div>
-            </form>
+            </div>
+
         </div>
 
     )
