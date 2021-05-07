0b111250d9e10cefb566904ef25e58586663590f
bonnici <bonnici@kth.se>
2021-05-07 15:31:48 +0200

    Changed css styles of html elements in GameItemYourTurn

--- techquiz/src/components/dashboard/GameItemYourTurn.js
+++ techquiz/src/components/dashboard/GameItemYourTurn.js
@@ -14,7 +14,7 @@ const GameItemYourTurn = ({game}) => {
                     {game[2].opponentName !== "" ? game[2].opponentName : "Random Player"}
                 </div>
                 <div className="gameitem-col">
-                    <Link to={'/game-landing/' + game[0]}><button className="right btn waves-effect waves-light #64b5f6 blue lighten-2">Play</button></Link>
+                    <Link to={'/game-landing/' + game[0]}><button className="right btn waves-effect waves-light #64b5f6 blue lighten-2 active-games-btn">Play</button></Link>
                 </div>
         </div>
             : <img src={"http://www.csc.kth.se/~cristi/loading.gif"} alt={"waiting for data"}/>
