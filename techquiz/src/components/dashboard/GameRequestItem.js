8522b7e8d289164f9f0d4de5b787680db109878b
Pelle Arn <perarn@kth.se>
2021-05-08 21:14:20 +0200

    fixed thumbsupfunc getting the wrong paramas

--- techquiz/src/components/dashboard/GameRequestItem.js
+++ techquiz/src/components/dashboard/GameRequestItem.js
@@ -18,8 +18,8 @@ const GameRequestItem = ({invitation, invitationID, acceptGameInvitation, reject
             <div className="thumbs-container">
               {/*  <button className="btn waves-effect waves-light #64b5f6 blue lighten-2 btn-full-height" onClick={() => acceptGameInvitation(invitationID)}>Accept</button>
                 <button className="btn waves-effect waves-light red lighten-2 btn-full-height" onClick={() => rejectGameInvitation(invitationID)}>Decline</button>*/}
-                <ThumbsUp className="thumbs-up" acceptFriendRequest={acceptGameInvitation}/>
-                <ThumbsDown className="thumbs-down" rejectFriendRequest={rejectGameInvitation}/>
+                <ThumbsUp className="thumbs-up" acceptAction={acceptGameInvitation} requestID={invitationID}/>
+                <ThumbsDown className="thumbs-down" rejectAction={rejectGameInvitation} requestID={invitationID}/>
             </div>
         </div>
     )
