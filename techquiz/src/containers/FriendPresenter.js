91710f27ba5b8080ed420775ba0a933aea65a725
Pelle Arn <perarn@kth.se>
2021-05-08 22:03:24 +0200

    small fix

--- techquiz/src/containers/FriendPresenter.js
+++ techquiz/src/containers/FriendPresenter.js
@@ -22,8 +22,6 @@ const mapStateToProps = (state) => {
     const friendRequests = state.firestore.data.receivedFriendRequests;
     const sentFriendRequests = state.firestore.data.sentFriendRequests;
 
-    console.log(state.firestore.data.friends, 'firens');
-
     return{
         auth: state.firebase.auth,
         friendSearch: state.friends.usernameFriends,
