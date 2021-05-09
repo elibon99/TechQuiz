d0e38dad63edc50e6f1eeb4a530d0eb815543090
Pelle Arn <perarn@kth.se>
2021-05-09 13:22:18 +0200

    started working on notifications componeont

---
+++ techquiz/src/components/Notifications/Notifications.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import FriendItem from "../friends/FriendItem";
+import {Link} from "react-router-dom";
+
+const Notifications = ({notifications}) => {
+    return (
+        <div className="card">
+            {notifications ? Object.entries(notifications).map((notification) => {
+                return (
+                    <li key={notification[0]}>
+                        <Link to={notification[1].linkTo} key={notification[0]}>
+                            Notification from: <i>{notification[1].fromUser}</i> :"{notification[1].notificationMessage}"
+                        </Link>
+                    </li>
+                )
+            }) : "nope"}
+        </div>
+    )
+}
+
+export default Notifications;
