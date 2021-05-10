838e3320bd55971e8acdf56eb6780be8b8d95e71
Pelle Arn <perarn@kth.se>
2021-05-10 15:33:58 +0200

    forwarded some methods

--- techquiz/src/components/gameSetup/FindGame.js
+++ techquiz/src/components/gameSetup/FindGame.js
@@ -2,7 +2,7 @@ import React from 'react'
 import QuickMatch from "./QuickMatch";
 import {Redirect} from 'react-router-dom';
 
-const FindGame = ({auth, addToQueue, restoreRedirectTo, userStats, user, matchQueue, createFriendGame, friends, setUsername, restoreRedirectToFriendGame, friendGameStatus, recentPlayers, friendsTemp, friendSearch}) => {
+const FindGame = ({acceptGameInvitation, rejectGameInvitation, createGameInvitation, auth, addToQueue, restoreRedirectTo, userStats, user, matchQueue, createFriendGame, friends, setUsername, restoreRedirectToFriendGame, friendGameStatus, recentPlayers, friendsTemp, friendSearch}) => {
     if(!auth.uid) {
         return <Redirect to="/signin"/>
     }
@@ -19,7 +19,7 @@ const FindGame = ({auth, addToQueue, restoreRedirectTo, userStats, user, matchQu
             <div><h5 className="page-title">Find a game</h5>
                 <div className="container find-game-margin">
                     <button className="btn blue lighten-1 z-depth-0" onClick={() => {addToQueue(userStats.mlRating)}}>Random Match</button>
-                    <QuickMatch friendsTemp={friendsTemp} setUsername={setUsername} recentPlayers={recentPlayers} friends={friends} createFriendGame={createFriendGame} friendGameStatus={friendGameStatus} restoreRedirectTo={restoreRedirectToFriendGame}/>
+                    <QuickMatch acceptGameInvitation={acceptGameInvitation} rejectGameInvitation={rejectGameInvitation} createGameInvitation={createGameInvitation} friendsTemp={friendsTemp} setUsername={setUsername} recentPlayers={recentPlayers} friends={friends} createFriendGame={createFriendGame} friendGameStatus={friendGameStatus} restoreRedirectTo={restoreRedirectToFriendGame}/>
                 </div>
             </div>:
                 <img src={"http://www.csc.kth.se/~cristi/loading.gif"} alt={"waiting for data"}/>
