ec965fe5a5136f43194462eb2aa356b88e37c2a2
bonnici <bonnici@kth.se>
2021-05-10 15:40:22 +0200

    Changed layout of notifications

--- techquiz/src/components/Notifications/Notifications.js
+++ techquiz/src/components/Notifications/Notifications.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import FriendItem from "../friends/FriendItem";
-import {Link} from "react-router-dom";
 
 const Notifications = ({notifications}) => {
     return (
-        <div className="card">
-            {notifications ? Object.entries(notifications).map((notification) => {
-                return (
-                    <li key={notification[0]}>
-                        <Link to={notification[1].linkTo} key={notification[0]}>
-                            Notification from: <i>{notification[1].fromUser}</i> :"{notification[1].notificationMessage}"
-                        </Link>
+        <div id="nots" className="notification-dropdown">
+            {/*{notifications ? Object.entries(notifications).map((notification) => {*/}
+            {/*    return (*/}
+            {/*        <li key={notification[0]}>*/}
+            {/*            <Link to={notification[1].linkTo} key={notification[0]}>*/}
+            {/*                Notification from: <i>{notification[1].fromUser}</i> :"{notification[1].notificationMessage}"*/}
+            {/*            </Link>*/}
+            {/*        </li>*/}
+            {/*    )*/}
+            {/*}) : "nope"}*/}
+                <a href="!#" className="notification-item">
+                    <li to={'/profile'}>
+                        Notification from elias
                     </li>
-                )
-            }) : "nope"}
+                </a>
+                <a href="!#" className="notification-item">
+                    <li to={'/profile'}>
+                        Notification from elias
+                    </li>
+                </a>
+                <a href="!#" className="notification-item">
+                    <li to={'/profile'}>
+                        Notification from elias
+                    </li>
+                </a>
+
         </div>
     )
 }
