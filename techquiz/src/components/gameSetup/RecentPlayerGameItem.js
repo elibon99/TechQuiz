a8c91640463468be6954e501e30d8c9c33e42ed5
bonnici <bonnici@kth.se>
2021-05-10 15:42:20 +0200

    Changed layout of recent player game item

--- techquiz/src/components/gameSetup/RecentPlayerGameItem.js
+++ techquiz/src/components/gameSetup/RecentPlayerGameItem.js
@@ -5,11 +5,11 @@ const RecentPlayerGameItem = ({opponentInfo, createFriendGame}) => {
     return(
         opponentInfo ?
             <div className="gameitem-container">
-                <div className="gameitem-col">
+                <div className="recent-player-item-opponent-title">
                     {opponentInfo.opponentName}
                 </div>
-                <div className="gameitem-col">
-                    <button className="right btn waves-effect waves-light #64b5f6 blue lighten-2" >Invite to a game</button>
+                <div className="recent-player-item-btn-container">
+                    <button className="right btn waves-effect waves-light #64b5f6 blue lighten-2 games-btn">Invite to a game</button>
                 </div>
             </div>
             : <img src={"http://www.csc.kth.se/~cristi/loading.gif"} alt={"waiting for data"}/>
