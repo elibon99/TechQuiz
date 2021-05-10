0631d574f236a1a93701a65b498fdebe642a36f9
bonnici <bonnici@kth.se>
2021-05-10 15:41:40 +0200

    Changed styling of friendgameitem

--- techquiz/src/components/gameSetup/FriendGameItem.js
+++ techquiz/src/components/gameSetup/FriendGameItem.js
@@ -5,19 +5,19 @@ const FriendGameItem = ({friendInfo, createFriendGame}) => {
     return(
         friendInfo ?
             <div className="gameitem-container">
-                <div className="gameitem-col">
+                <div className="friend-game-item-opponent-title">
                     {friendInfo.username}
                 </div>
-                <div className="gameitem-col">
+                <div className="friend-game-item-status">
+                    {friendInfo.inGameWith ? <h5 className="friend-game-item-title">Active game</h5> : ""}
+                </div>
+                <div className="btn-container">
                     {friendInfo.inGameWith ?
-                        <div className="friend-game-item-container">
-                        <h5 className="friend-game-item-title">You guys are in a game</h5>
-                            <Link to={'/game-landing/' + friendInfo.gamingID}>
-                            <button className="right btn waves-effect waves-light #64b5f6 blue lighten-2" >View Game</button>
-                            </Link>
-                        </div> : <button className="right btn waves-effect waves-light #64b5f6 blue lighten-2" onClick={() => {createFriendGame(friendInfo.userID, friendInfo.username)}}>Play</button>
-                    }
-
+                        <Link to={'/game-landing/' + friendInfo.gamingID}>
+                            <button className="right btn waves-effect waves-light #64b5f6 blue lighten-2 games-btn" >View</button>
+                        </Link>:
+                        <button className="right btn waves-effect waves-light #64b5f6 blue lighten-2 games-btn" onClick={() => {createFriendGame(friendInfo.userID, friendInfo.username)}}>Play</button>
+                        }
                 </div>
             </div>
             : <img src={"http://www.csc.kth.se/~cristi/loading.gif"} alt={"waiting for data"}/>
