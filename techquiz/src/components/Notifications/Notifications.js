c28a58afd01a236ce026377d2fbb6164f743d63e
bonnici <bonnici@kth.se>
2021-05-12 23:23:16 +0200

    Changed layout of notification component

--- techquiz/src/components/Notifications/Notifications.js
+++ techquiz/src/components/Notifications/Notifications.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
+import moment from "moment";
+import ThumbsUp from "../friends/ThumbsUp";
+import ThumbsDown from "../friends/ThumbsDown";
 
-const Notifications = ({notifications}) => {
+const Notifications = ({notifications, acceptGameInvitation, rejectGameInvitation, acceptFriendRequest, rejectFriendRequest}) => {
     notifications ? console.log(notifications, "notifications") : console.log("no notifications");
 
     return (
@@ -22,7 +25,28 @@ const Notifications = ({notifications}) => {
                 return(
                     <li id="nots-item" key={notification[0]} className="notification-item">
                         <NavLink to={notification[1].linkTo}>
-                            {notification[1].notificationMessage} from {notification[1].fromUser}
+                            <div className="notification-item-container">
+                                <div className="notification-item-logo">
+                                    <img className="notification-user-pic" src={notification[1].fromUserPhotoURL} alt="notification-logo"/>
+                                </div>
+                                <div className="notification-info">
+                                    <div className='notification-title-request-container'>
+                                        <h6 className="notification-title">
+                                            {notification[1].notificationType === "incomingFriendRequest" ? "Friend Request" : notification[1].notificationType === "acceptedFriendRequest" ?
+                                            "Accepted friend request" : notification[1].notificationType === "incomingGameInvitation" ? "Game Invitation" : notification[1].notificationType === "acceptedGameInvitation" ?
+                                            "Accepted game invitation" : ""}
+                                        </h6>
+                                        {notification[1].notificationType === "incomingFriendRequest" || notification[1].notificationType === "incomingGameInvitation" ?
+                                            <div className="thumbs-container">
+                                                <ThumbsUp className="thumbs-up" acceptAction={notification[1].notificationType === "incomingFriendRequest" ? acceptFriendRequest : notification[1].notificationType === "incomingGameInvitation" ? acceptGameInvitation : ""} />
+                                                <ThumbsDown className="thumbs-down" rejectAction={notification[1].notificationType === "incomingFriendRequest" ? acceptFriendRequest : notification[1].notificationType === "incomingGameInvitation" ? acceptGameInvitation : ""}/>
+                                            </div> : ""}
+
+                                    </div>
+                                    <h6 className="notification-date">{moment(notification[1].createdAt.toDate()).fromNow()}</h6>
+                                    <h6 className="notification-message"> {notification[1].notificationMessage} from {notification[1].fromUser}</h6>
+                                </div>
+                            </div>
                         </NavLink>
                     </li>
                 )
