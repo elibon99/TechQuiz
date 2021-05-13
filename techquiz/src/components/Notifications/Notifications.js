eca4721fefb0e179563cabaccc523f16e9c24289
bonnici <bonnici@kth.se>
2021-05-13 13:33:58 +0200

    Made notification item render default photo if user haven't uploaded a profile img

--- techquiz/src/components/Notifications/Notifications.js
+++ techquiz/src/components/Notifications/Notifications.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 import moment from "moment";
-import ThumbsUp from "../friends/ThumbsUp";
-import ThumbsDown from "../friends/ThumbsDown";
+import NotificationSenderImg from "./NotificationSenderImg";
 
 const Notifications = ({notifications, acceptGameInvitation, rejectGameInvitation, acceptFriendRequest, rejectFriendRequest}) => {
     notifications ? console.log(notifications, "notifications") : console.log("no notifications");
 
     return (
         <div id="nots" className="notification-dropdown z-depth-3">
-            {/*{notifications ? Object.entries(notifications).map((notification) => {*/}
-            {/*    return (*/}
-            {/*        <li key={notification[0]}>*/}
-            {/*            <Link to={notification[1].linkTo} key={notification[0]}>*/}
-            {/*                Notification from: <i>{notification[1].fromUser}</i> :"{notification[1].notificationMessage}"*/}
-            {/*            </Link>*/}
-            {/*        </li>*/}
-            {/*    )*/}
-            {/*}) : "nope"}*/}
             <div className="dropdown-header">
                 <h6>Notifications</h6>
             </div>
@@ -27,7 +17,9 @@ const Notifications = ({notifications, acceptGameInvitation, rejectGameInvitatio
                         <NavLink to={notification[1].linkTo}>
                             <div className="notification-item-container">
                                 <div className="notification-item-logo">
-                                    <img className="notification-user-pic" src={notification[1].fromUserPhotoURL} alt="notification-logo"/>
+                                    {notification[1].fromUserPhotoURL ? <img className="notification-user-pic" src={notification[1].fromUserPhotoURL} alt="notification-logo"/> :
+                                    <NotificationSenderImg className="notification-sender-img"/>}
+
                                 </div>
                                 <div className="notification-info">
                                     <div className='notification-title-request-container'>
@@ -36,12 +28,6 @@ const Notifications = ({notifications, acceptGameInvitation, rejectGameInvitatio
                                             "Accepted friend request" : notification[1].notificationType === "incomingGameInvitation" ? "Game Invitation" : notification[1].notificationType === "acceptedGameInvitation" ?
                                             "Accepted game invitation" : ""}
                                         </h6>
-                                        {notification[1].notificationType === "incomingFriendRequest" || notification[1].notificationType === "incomingGameInvitation" ?
-                                            <div className="thumbs-container">
-                                                <ThumbsUp className="thumbs-up" acceptAction={notification[1].notificationType === "incomingFriendRequest" ? acceptFriendRequest : notification[1].notificationType === "incomingGameInvitation" ? acceptGameInvitation : ""} />
-                                                <ThumbsDown className="thumbs-down" rejectAction={notification[1].notificationType === "incomingFriendRequest" ? acceptFriendRequest : notification[1].notificationType === "incomingGameInvitation" ? acceptGameInvitation : ""}/>
-                                            </div> : ""}
-
                                     </div>
                                     <h6 className="notification-date">{moment(notification[1].createdAt.toDate()).fromNow()}</h6>
                                     <h6 className="notification-message"> {notification[1].notificationMessage} from {notification[1].fromUser}</h6>
@@ -51,25 +37,6 @@ const Notifications = ({notifications, acceptGameInvitation, rejectGameInvitatio
                     </li>
                 )
             }): <h5>No current notification</h5>}
-
-
-       {/*         <a href="!#" className="notification-item">
-                    <li to={'/profile'}>
-                        Notification from elias
-                    </li>
-                </a>
-                <a href="!#" className="notification-item">
-                    <li to={'/profile'}>
-                        Notification from elias
-                    </li>
-                </a>
-                <a href="!#" className="notification-item">
-                    <li to={'/profile'}>
-                        Notification from elias
-                    </li>
-                </a>*/}
-
-
         </div>
     )
 }
