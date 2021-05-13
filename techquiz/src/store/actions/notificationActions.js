a18d8af4dce15771a686f10016d61619caf5cfbb
bonnici <bonnici@kth.se>
2021-05-13 15:50:28 +0200

    Added action for notifications

---
+++ techquiz/src/store/actions/notificationActions.js
@@ -0,0 +1,17 @@
+
+
+export const clearNotifications = () => {
+    return (dispatch, getState, {getFirebase, getFirestore}) => {
+        const firestore = getFirestore();
+        const uid = getState().firebase.auth.uid;
+        firestore.collection('notifications').where('toUserID', '==', uid).get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    doc.ref.delete()
+                        .then(() => console.log("Successfully deleted notification"))
+                        .catch((error) => console.log("Failed to delete notification"));
+                })
+            }).catch((error) => console.log("failed to clear all notifications"));
+    }
+}
+
