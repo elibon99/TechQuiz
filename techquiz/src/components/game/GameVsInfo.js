6c08686b361ee90e6d82648895040fe7e28da463
bonnici <bonnici@kth.se>
2021-05-14 22:41:23 +0200

    Added profile image to game vs info

--- techquiz/src/components/game/GameVsInfo.js
+++ techquiz/src/components/game/GameVsInfo.js
@@ -3,11 +3,13 @@ import React from 'react';
 const GameVsInfo = ({game, opponent, profile, userStat,score}) => {
 
     return(
+        (profile && opponent) ?
         <div className="card-content">
             <div className="container">
             <div className="row flex">
                 <div className="col s12 m5 player-content">
-                    <i className="large material-icons">account_circle</i>
+                    {profile.photoURL? <img className="profile-info-pic" src={profile.photoURL} alt="profile-pic"/>:
+                        <i className="large material-icons profile-standard-pic">account_circle</i>}
                     <h5>
                         {profile.userName}
                     </h5>
@@ -19,7 +21,8 @@ const GameVsInfo = ({game, opponent, profile, userStat,score}) => {
                     <span>{score.userScore}-{score.opponentScore}</span>
                 </div>
                 <div className="col s12 m5 player-content">
-                    <i className="large material-icons">account_circle</i>
+                    {opponent.photoURL ? <img className="profile-info-pic" src={opponent.photoURL} alt="profile-pic"/>:
+                        <i className="large material-icons profile-standard-pic">account_circle</i>}
                     {opponent ?
                         <div>
                         <h5>
@@ -32,7 +35,7 @@ const GameVsInfo = ({game, opponent, profile, userStat,score}) => {
                 </div>
             </div>
             </div>
-        </div>
+        </div> : <img src={"http://www.csc.kth.se/~cristi/loading.gif"} alt={"waiting for data"}/>
     )
 }
 
