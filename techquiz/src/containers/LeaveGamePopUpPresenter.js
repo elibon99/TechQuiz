9ac020d6f9ac08498ae44ed14f0b6d55da611d31
bonnici <bonnici@kth.se>
2021-05-14 22:44:48 +0200

    Mapped dispatch to leave game pop up presenter

---
+++ techquiz/src/containers/LeaveGamePopUpPresenter.js
@@ -0,0 +1,25 @@
+import {connect} from "react-redux";
+import LeaveGamePopUp from "../components/Notifications/LeaveGamePopUp";
+import {forfeitGameSet, stopTimer} from "../store/actions/gameActions";
+
+const mapStateToProps = (state, ownProps) => {
+    const lengthOfPath = window.location.pathname.length;
+    const gameID = window.location.pathname.substring(14, lengthOfPath);
+    console.log(gameID);
+    return {
+        gameID: gameID,
+        uid: state.firebase.auth.uid
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+        forfeitGameSet: (gameID) => dispatch(forfeitGameSet(gameID)),
+        stopTimer: () => dispatch(stopTimer())
+    }
+}
+
+
+const LeaveGamePopUpPresenter = connect(mapStateToProps, mapDispatchToProps)(LeaveGamePopUp);
+
+export default LeaveGamePopUpPresenter;
