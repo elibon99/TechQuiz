671ed62382b3279e0ad2ca1d82e1fd798f93b77c
bonnici <bonnici@kth.se>
2021-05-15 23:36:34 +0200

    Changed layout of game finished

--- techquiz/src/components/game/GameFinished.js
+++ techquiz/src/components/game/GameFinished.js
@@ -2,49 +2,61 @@ import React from 'react';
 import {Link, Redirect} from "react-router-dom";
 import ThumbsUp from "../friends/ThumbsUp";
 import ThumbsDown from "../friends/ThumbsDown";
+import Trophy from "./Trophy";
 
 const GameFinished = ({weAreInGameID, weAreInAGame, itsMyTurn, auth, whoWon, userStats, opponentCredentials, username, createGameInvitation, opponentHasBeenInvited, iGotInvitedByOpponent, acceptGameInvitation, rejectGameInvitation, reqID}) => {
     if(!auth.uid) {
         return <Redirect to="/signin"/>
     }
     return (
-        <div className="container game-finished-container">
-            <h3 className="center">Game Finished</h3>
+        <div>
+        <div className="container">
+            <div className="card">
+                <div className="card-content">
+                    <div className="game-finished-container">
+                    <h3 className="game-finished-title">GAME FINISHED</h3>
 
-            <h5 className="center">{whoWon ? whoWon : ""}</h5>
+                    <div className="who-won-container">
+                        <Trophy className="trophy"/>
+                        <div className="game-who-won-text">{whoWon ? whoWon : ""}</div>
+                    </div>
+                    <br/>
+                    <br/>
 
-            <br/>
-            <br/>
+                    <p className="center">{username ? username : ""} rating: {userStats ? userStats.mlRating : ""}</p>
+                    <p className="center">{opponentCredentials ? opponentCredentials.username : ""} rating: {opponentCredentials ? opponentCredentials.rating : ""}</p>
 
-            <p className="center">{username ? username : ""} rating: {userStats ? userStats.mlRating : ""}</p>
-            <p className="center">{opponentCredentials ? opponentCredentials.username : ""} rating: {opponentCredentials ? opponentCredentials.rating : ""}</p>
-
-            <div className="container game-finished-btn">
-                {(!opponentHasBeenInvited && !iGotInvitedByOpponent && !weAreInAGame) ?
-                        <button className="btn blue lighten-1 z-depth-0 play-button" onClick={() => createGameInvitation(opponentCredentials.userID, opponentCredentials.username)}>Invite to a rematch</button>
-                    :
-                    opponentHasBeenInvited ?
-                    <button disabled={true} className="btn blue lighten-1 z-depth-0 play-button">Has already been invited</button>
-                        : iGotInvitedByOpponent ?
-                        <div className="game-finished-accept-invitation">
-                            <h6> {opponentCredentials.username} invited you to a new game. Accept?</h6>
-                            <div className="thumbs-container">
-                                <ThumbsUp className="thumbs-up" acceptAction={acceptGameInvitation} requestID={reqID[0][0]}/>
-                                <ThumbsDown className="thumbs-down" rejectAction={rejectGameInvitation} requestID={reqID[0][0]}/>
-                            </div>
-                        </div>
-                        : itsMyTurn ? <Link to={'/game-landing/' + weAreInGameID}>
-                            <button className="btn blue lighten-1 z-depth-0 play-button"> In game. Your turn.</button>
-                        </Link> : <Link to={'/game-landing/' + weAreInGameID}>
-                            <button className="btn blue lighten-1 z-depth-0 play-button"> In game. Their turn.</button>
+                    <div className="container game-finished-btn">
+                        {(!opponentHasBeenInvited && !iGotInvitedByOpponent && !weAreInAGame) ?
+                            <button className="btn blue lighten-1 z-depth-0 play-button" onClick={() => createGameInvitation(opponentCredentials.userID, opponentCredentials.username)}>Invite to a rematch</button>
+                            :
+                            opponentHasBeenInvited ?
+                                <button disabled={true} className="btn blue lighten-1 z-depth-0 play-button">Has already been invited</button>
+                                : iGotInvitedByOpponent ?
+                                <div className="gameitem-container">
+                                    <div className="game-invitation-text"> {opponentCredentials.username} invited you to a new game. Accept?</div>
+                                    <div className="thumbs-container thumbs-container-game-invitation">
+                                        <ThumbsUp className="thumbs-up" acceptAction={acceptGameInvitation} requestID={reqID[0][0]}/>
+                                        <ThumbsDown className="thumbs-down" rejectAction={rejectGameInvitation} requestID={reqID[0][0]}/>
+                                    </div>
+                                </div>
+                                : itsMyTurn ? <Link to={'/game-landing/' + weAreInGameID}>
+                                    <button className="btn blue lighten-1 z-depth-0 play-button"> In game. Your turn.</button>
+                                </Link> : <Link to={'/game-landing/' + weAreInGameID}>
+                                    <button className="btn blue lighten-1 z-depth-0 play-button"> In game. Their turn.</button>
+                                </Link>
+                        }
+                    </div>
+                    <div className="container game-finished-btn">
+                        <Link to='/'>
+                            <button className="btn blue lighten-1 z-depth-0 play-button">Return to your profile</button>
                         </Link>
-                }
+                    </div>
+                    </div>
+                </div>
             </div>
-            <div className="container game-finished-btn">
-                <Link to='/'>
-                    <button className="btn blue lighten-1 z-depth-0 play-button">Return to your profile</button>
-                </Link>
-            </div>
+
+        </div>
         </div>
     )
 }
