f56c0d745338a27149b99b245f8812b32395356c
Pelle Arn <perarn@kth.se>
2021-05-15 22:32:34 +0200

    small fix

--- techquiz/src/components/leaderboard/Ranking.js
+++ techquiz/src/components/leaderboard/Ranking.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Ranking = ({ratings, title, type, users}) => {
     return(
         <div className="card profile-info-card">
-            <div className="card-content">
-            <h5>{title}</h5>
-            {(ratings && users) ?
-            <ol>
-                {ratings && ratings.map((rating) => {
-                    return (
-                        <li key={rating.id}>{(type === "slScore" && users[rating.id]) ? users[rating.id].userName : rating.username}  {type === "slScore" ? rating.slScore : rating.rating}</li>
-                    )
-                })}
-            </ol> :
-            <img src={"http://www.csc.kth.se/~cristi/loading.gif"} alt={"waiting for data"}/>}
-            </div>
+            <Link to={"/leaderboard/" + title} className="leaderboard-category-card">
+                <div className="card-content">
+                <h5>{title}</h5>
+                {(ratings && users) ?
+                <ol>
+                    {ratings && ratings.map((rating) => {
+                        return (
+                            <li key={rating.id}>{(type === "slScore" && users[rating.id]) ? users[rating.id].userName : rating.username}  {type === "slScore" ? rating.slScore : rating.rating}</li>
+                        )
+                    })}
+                </ol> :
+                <img src={"http://www.csc.kth.se/~cristi/loading.gif"} alt={"waiting for data"}/>}
+                </div>
+            </Link>
         </div>
     )
 }
