c568a9d030e984d00bcb28131f9a1c9ea6495ede
Pelle Arn <perarn@kth.se>
2021-05-15 22:32:11 +0200

    fixed coditional rendering for all different categorytypes

--- techquiz/src/components/leaderboard/LeaderboardPerCategory.js
+++ techquiz/src/components/leaderboard/LeaderboardPerCategory.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
-const leaderboardPerCategory = ({category, categoryScore}) => {
+const leaderboardPerCategory = ({category, categoryScore, users}) => {
     return(
         <div className="card profile-info-card">
             <div className="card-content">
                 <h5>{category}</h5>
                 {categoryScore ?
                     <ol>
-                        {categoryScore && Object.entries(categoryScore).map((score) => {
+                        {category === "Singleplayer score" ? Object.entries(categoryScore).map((score) => {
                             return (
-                                <li key={score[0]}>{score[1].username}  {score[1].score}</li>
+                                <li key={score[0]}> {users[score[0]] !== undefined ? users[score[0]].userName : ""} {score[1].slScore} </li>
+                            )
+                        }) : category === "Multiplayer rating" ? Object.entries(categoryScore).map((score) => {
+                            return (
+                                <li key={score[0]}>{score[1].username} {score[1].rating}  </li>
+                            )
+                        }) : (categoryScore) && Object.entries(categoryScore).map((score) => {
+                            return (
+                                <li key={score[0]}>{score[1].username} {score[1].score}  </li>
                             )
                         })}
                     </ol> :
