76995a5de42abbdc639dcdf689c5c035c03606fc
Pelle Arn <perarn@kth.se>
2021-05-15 21:09:11 +0200

    small fix

--- techquiz/src/components/dashboard/GameInvitations.js
+++ techquiz/src/components/dashboard/GameInvitations.js
@@ -13,9 +13,11 @@ const GameInvitations = ({gameInvitations, acceptGameInvitation, rejectGameInvit
             <div className="card-content profile-info-card-content">
                 <h5 className="profile-info-card-title">Game Invitations</h5>
                 <div className="profile-info-game-invitations-container">
+                    <div>
                         {gameInvitations ? Object.entries(gameInvitations).map((entry) => {
                             return <GameRequestItem key={entry[0]} invitationID={entry[0]} acceptGameInvitation={acceptGameInvitation} rejectGameInvitation={rejectGameInvitation} invitation={entry[1]} />
-                        }) : <h6 className="profile-info-no-data-title">You don't have any game invitations at the moment</h6>}
+                        }) : <h6 className="profile-info-no-data-title">You don't have any incoming game invitations at the moment</h6>}
+                    </div>
                 </div>
             </div>
 
