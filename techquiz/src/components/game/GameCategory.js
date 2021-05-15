bfcb9128ebf2e4c3d3ae1b8d40c20933330670e2
bonnici <bonnici@kth.se>
2021-05-15 22:26:22 +0200

    Changed route guarding of game category

--- techquiz/src/components/game/GameCategory.js
+++ techquiz/src/components/game/GameCategory.js
@@ -2,27 +2,27 @@ import React from 'react';
 import GameVsCategoryInfo from "./GameVsCategoryInfo";
 import {Link, Redirect} from "react-router-dom";
 
-const GameCategory = ({game, opponent, profile, userStat, score, isYourTurn, auth, localGame, fetchQuestions, gamingID, hasChosenCategory}) =>  {
+const GameCategory = ({game, opponent, generatedCategories, profile, userStat, score, isYourTurn, auth, localGame, fetchQuestions, gamingID, hasChosenCategory}) =>  {
     const [selectedCategories, setSelectedCategories]= React.useState("");
     if(!auth.uid) {
         return <Redirect to="/signin"/>
     }
 
     return (
-        (game && userStat && localGame.selectedCategories) ?
+        (game && userStat && generatedCategories) ?
         <div className="container">
             <div className="card game-landing-container">
                 <GameVsCategoryInfo game={game} opponent={opponent} profile={profile} userStat={userStat} hasChosenCategory={hasChosenCategory}/>
                 <div className="card-content">
                     <div className="container">
                         <div className="row flex">
-                    {(localGame.selectedCategories && !hasChosenCategory) ? localGame.selectedCategories.map((category => {
+                    {(generatedCategories && !hasChosenCategory) ? Object.entries(generatedCategories).map((category => {
                         return (
-                            <div key={category.tags}  className="col s12 m6 game-category-col">
-                                <div id={category.tags} onClick={e => {setSelectedCategories(e.target.id)}} className="card category-title-container" tabIndex="1">
-                                    <div id={category.tags} className="quiz-landing-card quiz-landing-card-title" onClick={e => {setSelectedCategories(e.target.id)}}>
-                                        <img id={category.tags} className="category-background-image" onClick={e => {setSelectedCategories(e.target.id)}} src={category.iconSrc} alt={category.category}/>
-                                            {category.category}
+                            <div key={category[1].tags}  className="col s12 m6 game-category-col">
+                                <div id={category[1].tags} onClick={e => {setSelectedCategories(e.target.id)}} className="card category-title-container" tabIndex="1">
+                                    <div id={category[1].tags} className="quiz-landing-card quiz-landing-card-title" onClick={e => {setSelectedCategories(e.target.id)}}>
+                                        <img id={category[1].tags} className="category-background-image" onClick={e => {setSelectedCategories(e.target.id)}} src={category[1].iconSrc} alt={category[1].category}/>
+                                            {category[1].category}
                                     </div>
                                 </div>
                             </div>)
@@ -41,7 +41,7 @@ const GameCategory = ({game, opponent, profile, userStat, score, isYourTurn, aut
                         </div>
                 </div>
             </div>
-        </div> : <img src={"http://www.csc.kth.se/~cristi/loading.gif"} alt={"waiting for data"}/>
+        </div> : <img className='loading-wheel-general-view' src={"http://www.csc.kth.se/~cristi/loading.gif"} alt={"waiting for data"}/>
     )
 }
 
