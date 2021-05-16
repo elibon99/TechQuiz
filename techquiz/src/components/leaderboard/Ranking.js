7cc78691056f20e977aa40eabf0a203f931d3043
bonnici <bonnici@kth.se>
2021-05-16 18:24:09 +0200

    Changed style of ranking component

--- techquiz/src/components/leaderboard/Ranking.js
+++ techquiz/src/components/leaderboard/Ranking.js
@@ -1,23 +1,76 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
+import Trophy from "./Trophy";
+import AccountImg from "../Notifications/AccountImg";
 
 const Ranking = ({ratings, title, type, users}) => {
     return(
         <div className="card profile-info-card">
-            <Link to={"/leaderboard/" + title} className="leaderboard-category-card">
-                <div className="card-content">
-                <h5>{title}</h5>
-                {(ratings && users) ?
-                <ol>
-                    {ratings && ratings.map((rating) => {
-                        return (
-                            <li key={rating.id}>{(type === "slScore" && users[rating.id]) ? users[rating.id].userName : rating.username}  {type === "slScore" ? rating.slScore : rating.rating}</li>
-                        )
-                    })}
-                </ol> :
-                <img src={"http://www.csc.kth.se/~cristi/loading.gif"} alt={"waiting for data"}/>}
-                </div>
-            </Link>
+            <div className="leaderboard-small-category-container">
+                <h6 className="leaderboard-small-title">{title.toUpperCase()}</h6>
+                <table>
+                    <thead>
+                        <tr>
+                            <th className="width-col1" scope="col">Rank</th>
+                            <th className="width-col2" scope="col">Player</th>
+                            <th className="width-col3" scope="col">{title === "Multiplayer rating" ? "Rating" : "Score"}</th>
+                        </tr>
+                    </thead>
+                    {ratings && users ?
+                     <tbody>
+                        {title === "Singleplayer score" ? ratings.map((rating, index) => {
+                            return (
+                                <NavLink className="navlink-leaderboard" to={'/profile-preview/' + rating.id}>
+                                    <tr key={rating.id}>
+                                        <th className="width-col1">
+                                            <div className="ranking-trohpy-container">
+                                                {index+1 === 1 ?
+                                                    <span><Trophy className="trophy"/></span>
+                                                    : index+1 === 2 ?
+                                                        <span><Trophy className="trophy trophy-silver"/></span>
+                                                        : index+1 === 3 ?
+                                                            <span><Trophy className="trophy trophy-bronze"/></span>
+                                                            : ""} {index+1}
+                                            </div>
+                                        </th>
+                                        <th className="width-col2">
+                                            <div className="username-photo-container">
+                                                {(users[rating.id] !== undefined) && (users[rating.id].photoURL !== null) ?
+                                                    <img className="profile-pic-leaderboard" src={users[rating.id].photoURL} alt="profile-pic"/>:
+                                                    <AccountImg className="default-pic-leaderboard"/>}
+                                                {users[rating.id] !== undefined ? users[rating.id].userName : ""}
+                                            </div>
+                                        </th>
+                                        <th className="width-tbody-col3">{rating.slScore}</th>
+                                    </tr>
+                                </NavLink>
+                            )
+                        }) : ratings.map((rating,index) => {
+                            return(
+                                <NavLink className="navlink-leaderboard" to={'/profile-preview/' + rating.id}>
+                                    <tr key={rating.id}>
+                                        <th className="width-col1"><div className="ranking-trohpy-container">{index+1 === 1 ?
+                                            <span><Trophy className="trophy"/></span> : index+1 === 2 ?
+                                                <span><Trophy className="trophy trophy-silver"/></span> : index+1 === 3 ?
+                                                    <span><Trophy className="trophy trophy-bronze"/></span> : ""} {index+1}</div></th>
+                                        <th className="width-col2">
+                                            <div className="username-photo-container">
+                                                {(users[rating.id] !== undefined) && (users[rating.id].photoURL !== null) ?
+                                                    <img className="profile-pic-leaderboard" src={users[rating.id].photoURL} alt="profile-pic"/>:
+                                                    <AccountImg className="default-pic-leaderboard"/>}
+                                                {users[rating.id] !== undefined ? users[rating.id].userName : ""}
+                                            </div>
+                                        </th>
+                                        <th className="width-tbody-col3">{rating.rating}</th>
+
+                                    </tr>
+                                </NavLink>
+                            )
+                        })
+                        }
+                     </tbody> : <img src={"http://www.csc.kth.se/~cristi/loading.gif"} alt={"waiting for data"}/> }
+                </table>
+            </div>
         </div>
     )
 }
