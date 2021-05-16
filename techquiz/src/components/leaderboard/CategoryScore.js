0a34912d72355fc6beb75e454e3af8621d9765d0
bonnici <bonnici@kth.se>
2021-05-16 18:22:41 +0200

    Changed style of category score component

--- techquiz/src/components/leaderboard/CategoryScore.js
+++ techquiz/src/components/leaderboard/CategoryScore.js
@@ -1,25 +1,57 @@
 import React from 'react';
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
+import Trophy from "./Trophy";
+import AccountImg from "../Notifications/AccountImg";
 
-const CategoryScore = ({scores, title}) => {
+const CategoryScore = ({scores, title, users}) => {
     return(
-        <div className="col s12 m4">
-            <Link to={"/leaderboard/" + title} className="leaderboard-category-card">
-            <div className="card profile-info-card">
-                <div className="card-content">
-                    <h5>{title}</h5>
-                    {scores ?
-                        <ol>
-                            {scores && scores.map((score) => {
-                                return (
-                                    <li key={score.id}>{score.username}  {score.score}</li>
-                                )
-                            })}
-                        </ol> :
-                        <img src={"http://www.csc.kth.se/~cristi/loading.gif"} alt={"waiting for data"}/>}
-                </div>
+        <div className="col s12 m12 l6 xl4">
+            <div className="card leaderboard-card">
+            <div className="leaderboard-small-category-container category-score-small-container">
+                <h6 className="leaderboard-small-title">{title}</h6>
+                <table>
+                    <thead>
+                    <tr>
+                        <th className="width-col1" scope="col">Rank</th>
+                        <th className="width-col2" scope="col">Player</th>
+                        <th className="width-col3" scope="col">{title === "Multiplayer rating" ? "Rating" : "Score"}</th>
+                    </tr>
+                    </thead>
+                    {scores && users ?
+                        <tbody className="tbody-small-leaderboard">
+                        {scores.map((score, index) => {
+                            return (
+                                <NavLink className="navlink-leaderboard" to={'/profile-preview/' + score.id}>
+                                    <tr key={score.id}>
+                                        <th className="width-col1">
+                                            <div className="ranking-trohpy-container">
+                                                {index+1 === 1 ?
+                                                    <span><Trophy className="trophy"/></span>
+                                                    : index+1 === 2 ?
+                                                        <span><Trophy className="trophy trophy-silver"/></span>
+                                                        : index+1 === 3 ?
+                                                            <span><Trophy className="trophy trophy-bronze"/></span>
+                                                            : ""} {index+1}
+                                            </div>
+                                        </th>
+                                        <th className="width-col2">
+                                            <div className="username-photo-container">
+                                                {(users[score.id] !== undefined) && (users[score.id].photoURL !== null) ?
+                                                    <img className="profile-pic-leaderboard" src={users[score.id].photoURL} alt="profile-pic"/>:
+                                                    <AccountImg className="default-pic-leaderboard"/>}
+                                                {score.username !== undefined ? score.username : ""}
+                                            </div>
+                                        </th>
+                                        <th className="width-tbody-col3">{score.score}</th>
+                                    </tr>
+                                </NavLink>
+                            )
+                        })
+                        }
+                        </tbody> : <img src={"http://www.csc.kth.se/~cristi/loading.gif"} alt={"waiting for data"}/> }
+                </table>
             </div>
-            </Link>
+            </div>
         </div>
     )
 }
