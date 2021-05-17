587f04d2aba6fa95f6c3110bd831a4e035209c51
bonnici <bonnici@kth.se>
2021-05-17 22:49:16 +0200

    Fixed so that game invitations are fixed on client to improve speed

--- techquiz/src/store/actions/gameInvitationActions.js
+++ techquiz/src/store/actions/gameInvitationActions.js
@@ -51,10 +51,47 @@ export const createGameInvitation = (opponentID, opponentName) => {
 export const acceptGameInvitation = (invitationID) => {
     return(dispatch, getState, {getFirestore}) => {
         const firestore = getFirestore();
-
         firestore.collection('gameInvitations').doc(invitationID).update({
             isAccepted: true
-        }).then(() => dispatch({type: "ACCEPTED_INVITATION_SUCCESS"}))
+        }).then(() => {
+            firestore.collection('gameInvitations').doc(invitationID).get()
+                .then((docRef) => {
+                    firestore.collection('games').add({
+                        userID1: docRef.data().sentRequestID,
+                        user1Name: docRef.data().sentReqUserName,
+                        userID2: docRef.data().gotRequestID,
+                        user2Name: docRef.data().gotReqUserName,
+                        turn: docRef.data().sentRequestID,
+                        p1Score: 0,
+                        p2Score: 0,
+                        currentSet: "1",
+                        generatedCategories: null,
+                        shouldCreateNewGameSet : docRef.data().sentRequestID,
+                        amountOfPlayerLeft : 2,
+                        redirectTo: null,
+                        gameIsFinished: false,
+                        timeOfGameFinished: null,
+                        hasChosenCategory: false
+                    }).then((docRef2) => {
+                        firestore.collection('gameInvitations').doc(invitationID).delete()
+                            .then(() => {
+                                firestore.collection('notifications').add({
+                                    notificationMessage: "They accepted your game invitation",
+                                    toUser: docRef.data().sentReqUserName,
+                                    fromUser: docRef.data().gotReqUserName,
+                                    toUserID: docRef.data().sentRequestID,
+                                    fromUserID: docRef.data().gotRequestID,
+                                    linkTo: "/game-landing/" + docRef2.id,
+                                    createdAt: new Date(),
+                                    notificationType: "acceptedGameInvitation",
+                                    fromUserPhotoURL: docRef.data().gotReqPhotoURL,
+                                    requestID: null
+                                }).then()
+                                    .catch((error) => console.log(error, 'something went wrong updating notification collection friend cloud func'))
+                            }).catch((error) => console.log("Coulndt delete gameInvitation, ", error))
+                    }).catch((error) => console.log("Couldn't create game from invitation, ", error));
+                }).catch((error) => console.log("Something went wrong"))
+        })
             .catch((error) => dispatch({type: "ACCEPTED_INVITATION_FAILURE", error}))
     }
 }
@@ -70,7 +107,13 @@ export const rejectGameInvitation = (invitationID) => {
 
         firestore.collection('gameInvitations').doc(invitationID).update({
             isRejected: true
-        }).then(() => dispatch({type: "REJECTED_INVITATION_SUCCESS"}))
+        }).then(() => {
+            firestore.collection('gameInvitations').doc(invitationID).delete()
+                .then()
+                .catch((error) => console.log("Failed to delete gameInvitations"));
+            dispatch({type: "REJECTED_INVITATION_SUCCESS"});
+
+        })
             .catch((error) => dispatch({type: "REJECTED_INVITATION_FAILURE", error}))
     }
 }
