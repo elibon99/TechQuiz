f1e7459b66ceb6a8b4e07e5fe3ba75d9dada089a
bonnici <bonnici@kth.se>
2021-05-17 22:46:54 +0200

    Cleaned up code

--- techquiz/src/components/dashboard/GameRequestItem.js
+++ techquiz/src/components/dashboard/GameRequestItem.js
@@ -19,8 +19,6 @@ const GameRequestItem = ({invitation, invitationID, acceptGameInvitation, reject
             </Link>
 
             <div className="thumbs-container">
-              {/*  <button className="btn waves-effect waves-light #64b5f6 blue lighten-2 btn-full-height" onClick={() => acceptGameInvitation(invitationID)}>Accept</button>
-                <button className="btn waves-effect waves-light red lighten-2 btn-full-height" onClick={() => rejectGameInvitation(invitationID)}>Decline</button>*/}
                 <ThumbsUp className="thumbs-up" acceptAction={acceptGameInvitation} requestID={invitationID}/>
                 <ThumbsDown className="thumbs-down" rejectAction={rejectGameInvitation} requestID={invitationID}/>
             </div>
