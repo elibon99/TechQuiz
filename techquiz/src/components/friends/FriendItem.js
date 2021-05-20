0a13fc4cf02e99a4337c5bafdf278715cd72425e
bonnici <bonnici@kth.se>
2021-05-20 18:15:19 +0200

    Changed some styling

--- techquiz/src/components/friends/FriendItem.js
+++ techquiz/src/components/friends/FriendItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import AccountImg from "../Notifications/AccountImg";
 
 const FriendItem = ({user}) => {
     return (
-        <div className="col s12 m3 player-content">
+        <div className="col s6 m3 player-content">
             <div className="card card-friend-item">
                 <div className="card-content friends-card-padding">
                     {user.photoURL ? <img className="profile-pic-friend-frienditem" alt="profile-pic" src={user.photoURL}/>
-                    : <i className="medium material-icons">account_circle</i>}
+                    : <AccountImg className="friend-default-photo"/>}
                     <h6>{user.userName}</h6>
                 </div>
             </div>
